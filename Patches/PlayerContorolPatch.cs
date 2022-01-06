877e8e2ed975d1eb3a51cab27a7398aa3090c12f
tukasa0001 <tts13511@hotmail.com>
2022-01-06 15:42:43 +0900

    タブインテントを2から4に修正

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -14,114 +14,114 @@ using System.Threading;
 
 namespace TownOfHost
 {
-  [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.MurderPlayer))]
-  class MurderPlayerPatch
-  {
-    public static void Postfix(PlayerControl __instance, [HarmonyArgument(0)] PlayerControl target)
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.MurderPlayer))]
+    class MurderPlayerPatch
     {
-      if (!target.Data.IsDead)
-        return;
-      //When Bait is killed
-      if (target.Data.Role.Role == RoleTypes.Scientist && main.currentScientist == ScientistRole.Bait && AmongUsClient.Instance.AmHost
-      && __instance.PlayerId != target.PlayerId)
-      {
-        Thread.Sleep(150);
-        __instance.CmdReportDeadBody(target.Data);
-      }
-      else
-      //Terrorist
-      if (main.isTerrorist(target))
-      {
-        main.CheckTerroristWin(target.Data);
-      }
-    }
-  }
-  [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CheckMurder))]
-  class CheckMurderPatch
-  {
-    public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] PlayerControl target)
-    {
-      if (!AmongUsClient.Instance.AmHost) return false;
-      if (main.isSidekick(__instance))
-      {
-        var ImpostorCount = 0;
-        foreach (var pc in PlayerControl.AllPlayerControls)
+        public static void Postfix(PlayerControl __instance, [HarmonyArgument(0)] PlayerControl target)
         {
-          if (pc.Data.Role.Role == RoleTypes.Impostor &&
-             !pc.Data.IsDead) ImpostorCount++;
+            if (!target.Data.IsDead)
+                return;
+            //When Bait is killed
+            if (target.Data.Role.Role == RoleTypes.Scientist && main.currentScientist == ScientistRole.Bait && AmongUsClient.Instance.AmHost
+            && __instance.PlayerId != target.PlayerId)
+            {
+                Thread.Sleep(150);
+                __instance.CmdReportDeadBody(target.Data);
+            }
+            else
+            //Terrorist
+            if (main.isTerrorist(target))
+            {
+                main.CheckTerroristWin(target.Data);
+            }
         }
-        if (ImpostorCount > 0) return false;
-      }
-      if (main.isVampire(__instance) && !main.isBait(target))
-      { //キルキャンセル&自爆処理
-        __instance.RpcProtectPlayer(target, 0);
-        __instance.RpcMurderPlayer(target);
-        main.BitPlayers.Add(target.PlayerId, (__instance.PlayerId, 0f));
-        return false;
-      }
+    }
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CheckMurder))]
+    class CheckMurderPatch
+    {
+        public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] PlayerControl target)
+        {
+            if (!AmongUsClient.Instance.AmHost) return false;
+            if (main.isSidekick(__instance))
+            {
+                var ImpostorCount = 0;
+                foreach (var pc in PlayerControl.AllPlayerControls)
+                {
+                    if (pc.Data.Role.Role == RoleTypes.Impostor &&
+                         !pc.Data.IsDead) ImpostorCount++;
+                }
+                if (ImpostorCount > 0) return false;
+            }
+            if (main.isVampire(__instance) && !main.isBait(target))
+            { //キルキャンセル&自爆処理
+                __instance.RpcProtectPlayer(target, 0);
+                __instance.RpcMurderPlayer(target);
+                main.BitPlayers.Add(target.PlayerId, (__instance.PlayerId, 0f));
+                return false;
+            }
 
-      __instance.RpcMurderPlayer(target);
-      if (main.isFixedCooldown)
-      {
-        __instance.RpcProtectPlayer(target, 0);
-        __instance.RpcMurderPlayer(target);
-      }
-      return false;
-    }
-  }
-  [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.ReportDeadBody))]
-  class ReportDeadBodyPatch
-  {
-    public static bool Prefix(PlayerControl __instance)
-    {
-      if (main.IsHideAndSeek) return false;
-      if (AmongUsClient.Instance.AmHost)
-      {
-        foreach (var bp in main.BitPlayers)
-        {
-          foreach (var pc in PlayerControl.AllPlayerControls)
-          {
-            if (bp.Key == pc.PlayerId && !pc.Data.IsDead)
+            __instance.RpcMurderPlayer(target);
+            if (main.isFixedCooldown)
             {
-              pc.RpcMurderPlayer(pc);
-              main.PlaySoundRPC(bp.Value.Item1, Sounds.KillSound);
+                __instance.RpcProtectPlayer(target, 0);
+                __instance.RpcMurderPlayer(target);
             }
-          }
+            return false;
         }
-      }
-      main.BitPlayers = new Dictionary<byte, (byte, float)>();
-      return true;
     }
-  }
-  [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
-  class FixedUpdatePatch
-  {
-    public static void Postfix(PlayerControl __instance)
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.ReportDeadBody))]
+    class ReportDeadBodyPatch
     {
-      if (AmongUsClient.Instance.AmHost)
-      {
-        if (main.BitPlayers.ContainsKey(__instance.PlayerId))
+        public static bool Prefix(PlayerControl __instance)
         {
-          //__instance：キルされる予定のプレイヤー
-          //main.BitPlayers[__instance.PlayerId].Item1：キルしたプレイヤーのID
-          //main.BitPlayers[__instance.PlayerId].Item2：キルするまでの秒数
-          if (main.BitPlayers[__instance.PlayerId].Item2 >= 10)
-          {
-            byte vampireID = main.BitPlayers[__instance.PlayerId].Item1;
-            if (!__instance.Data.IsDead)
+            if (main.IsHideAndSeek) return false;
+            if (AmongUsClient.Instance.AmHost)
             {
-              __instance.RpcMurderPlayer(__instance);
-              main.PlaySoundRPC(vampireID, Sounds.KillSound);
+                foreach (var bp in main.BitPlayers)
+                {
+                    foreach (var pc in PlayerControl.AllPlayerControls)
+                    {
+                        if (bp.Key == pc.PlayerId && !pc.Data.IsDead)
+                        {
+                            pc.RpcMurderPlayer(pc);
+                            main.PlaySoundRPC(bp.Value.Item1, Sounds.KillSound);
+                        }
+                    }
+                }
             }
-            main.BitPlayers.Remove(__instance.PlayerId);
-          }
-          else
-          {
-            main.BitPlayers[__instance.PlayerId] =
-            (main.BitPlayers[__instance.PlayerId].Item1, main.BitPlayers[__instance.PlayerId].Item2 + Time.fixedDeltaTime);
-          }
+            main.BitPlayers = new Dictionary<byte, (byte, float)>();
+            return true;
         }
-      }
     }
-  }
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+    class FixedUpdatePatch
+    {
+        public static void Postfix(PlayerControl __instance)
+        {
+            if (AmongUsClient.Instance.AmHost)
+            {
+                if (main.BitPlayers.ContainsKey(__instance.PlayerId))
+                {
+                    //__instance：キルされる予定のプレイヤー
+                    //main.BitPlayers[__instance.PlayerId].Item1：キルしたプレイヤーのID
+                    //main.BitPlayers[__instance.PlayerId].Item2：キルするまでの秒数
+                    if (main.BitPlayers[__instance.PlayerId].Item2 >= 10)
+                    {
+                        byte vampireID = main.BitPlayers[__instance.PlayerId].Item1;
+                        if (!__instance.Data.IsDead)
+                        {
+                            __instance.RpcMurderPlayer(__instance);
+                            main.PlaySoundRPC(vampireID, Sounds.KillSound);
+                        }
+                        main.BitPlayers.Remove(__instance.PlayerId);
+                    }
+                    else
+                    {
+                        main.BitPlayers[__instance.PlayerId] =
+                        (main.BitPlayers[__instance.PlayerId].Item1, main.BitPlayers[__instance.PlayerId].Item2 + Time.fixedDeltaTime);
+                    }
+                }
+            }
+        }
+    }
 }
