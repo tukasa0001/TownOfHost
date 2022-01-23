cbe8845d5234e5bc081fa07ed8d81a6a51d12ac2
tukasa0001 <tts13511@hotmail.com>
2022-01-23 15:26:22 +0900

    崩壊一分前を切るとタイマーが赤文字になるよう変更

--- Patches/UnrestrictNumImpostor.cs
+++ Patches/UnrestrictNumImpostor.cs
@@ -112,6 +112,7 @@ namespace TownOfHost
                 int minutes = (int)timer / 60;
                 int seconds = (int)timer % 60;
                 string suffix = $" ({minutes:00}:{seconds:00})";
+                if(timer <= 60) suffix = "<color=#ff0000>" + suffix + "</color>";
 
                 __instance.PlayerCounter.text = currentText + suffix;
                 __instance.PlayerCounter.autoSizeTextContainer = true;
