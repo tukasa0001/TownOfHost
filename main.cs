3bda1af15b564d13614918a5c867318907a18cd3
tanakanira0118 <github@tanakanira.com>
2022-01-27 02:11:15 +0900

    beta3に変更

--- main.cs
+++ main.cs
@@ -19,7 +19,7 @@ namespace TownOfHost
         public const string PluginGuid = "com.emptybottle.townofhost";
         public const string PluginVersion = "1.4";
         public const VersionTypes PluginVersionType = VersionTypes.Beta;
-        public const string BetaVersion = "2";
+        public const string BetaVersion = "3";
         public const string BetaName = "Sabotage Master Beta";
         public static string VersionSuffix => PluginVersionType == VersionTypes.Beta ? "b #" + BetaVersion : "";
         public Harmony Harmony { get; } = new Harmony(PluginGuid);
