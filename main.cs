bbb608f5b040f0ec6752a4702289d42f698e2769
tanakanira0118 <github@tanakanira.com>
2022-02-02 17:17:57 +0900

    色を修正

--- main.cs
+++ main.cs
@@ -115,7 +115,7 @@ namespace TownOfHost
             string hexColor;
             roleColors.TryGetValue(role, out hexColor);
             MatchCollection matches = Regex.Matches(hexColor, "[0-9a-fA-F]{2}");
-            return new Color(Convert.ToInt32(matches[0].Value,16),Convert.ToInt32(matches[1].Value,16),Convert.ToInt32(matches[2].Value,16));
+            return new Color(Convert.ToInt32(matches[0].Value,16)/255f,Convert.ToInt32(matches[1].Value,16)/255f,Convert.ToInt32(matches[2].Value,16)/255f);
         }
         public static string getRoleColorCode(CustomRoles role)
         {
