04b66e17dd7b974ab6d64fc5d249783caf5eb142
yurinakira <84663668+yurinakira@users.noreply.github.com>
2022-02-02 16:16:52 +0900

    add loversInfo

--- main.cs
+++ main.cs
@@ -482,6 +482,7 @@ namespace TownOfHost
             writer.Write(WarlockCount);
             writer.Write(FoxCount);
             writer.Write(TrollCount);
+            writer.Write(LoversCount);
 
 
             writer.Write(IsHideAndSeek);
@@ -736,6 +737,7 @@ namespace TownOfHost
                 {lang.SnitchInfo, "タスクを完了させ、人外を暴け"},
                 {lang.BountyHunterInfo, "標的を確実に仕留めよう"},
                 {lang.WarlockInfo, "みんなを呪い殺そう"},
+                {lang.LoversInfo, "恋人と幸せに"},
                 //役職解説(長)
                 {lang.JesterInfoLong, "ジェスター:\n会議で追放されたときに単独勝利となる第三陣営の役職。追放されずにゲームが終了するか、キルされると敗北となる。"},
                 {lang.MadmateInfoLong, "狂人:\nインポスター陣営に属するが、インポスターが誰なのかはわからない。インポスターからも狂人が誰なのかはわからない。キルやサボタージュは使えないが、通気口を使うことができる。"},
@@ -752,6 +754,7 @@ namespace TownOfHost
                 {lang.WarlockInfoLong, "ウォーロック:\n変身をしている間に誰かをキルするとキルした人に呪いがかかる。その後にキルボタンを押すとキルされた人に１番近い人が（インポスターであろうと）呪われた人によりキルされる。"},
                 {lang.FoxInfoLong, "狐(HideAndSeek):\nトロールを除くいずれかの陣営が勝利したときに生き残っていれば、勝利した陣営に追加で勝利することができる。"},
                 {lang.TrollInfoLong, "トロール(HideAndSeek):\nインポスターにキルされたときに単独勝利となる。この場合、狐が生き残っていても狐は敗北となる。"},
+                {lang.LoversInfoLong, "ラバーズ(Lovers):\nTODO"},
                 //モード名
                 {lang.HideAndSeek, "HideAndSeek"},
                 {lang.NoGameEnd, "NoGameEnd"},
