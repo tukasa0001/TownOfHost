446820afb340c643472b5d82217ff00c98f1efa2
tukasa0001 <tts13511@hotmail.com>
2022-02-07 19:35:07 +0900

    MeetingHudのnull参照を修正

--- Patches/MeetingHudPatch.cs
+++ Patches/MeetingHudPatch.cs
@@ -163,6 +163,7 @@ namespace TownOfHost
     {
         public static void Postfix(MeetingHud __instance)
         {
+            if(AmongUsClient.Instance.GameMode == GameModes.FreePlay) return;
             foreach (var pva in __instance.playerStates)
             {
                 var RoleTextMeetingTransform = pva.NameText.transform.Find("RoleTextMeeting");
