db03e15d4ff152b0a2b0b52e676181eb35989bb9
tanakanira0118 <github@tanakanira.com>
2022-02-11 13:11:15 +0900

    ベータ名をName Revolution Bataに変更

--- main.cs
+++ main.cs
@@ -24,7 +24,7 @@ namespace TownOfHost
         public const string PluginVersion = "1.4";
         public const VersionTypes PluginVersionType = VersionTypes.Beta;
         public const string BetaVersion = "3";
-        public const string BetaName = "Mad Guardian Beta";
+        public const string BetaName = "Name Revolution Beta";
         public static string VersionSuffix => PluginVersionType == VersionTypes.Beta ? "b #" + BetaVersion : "";
         public Harmony Harmony { get; } = new Harmony(PluginGuid);
         public static BepInEx.Logging.ManualLogSource Logger;
