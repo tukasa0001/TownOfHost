ca9ad1e50c38bdc97b03eed870d18e53a9106703
そうくん <daoyecangma@gmail.com>
2022-02-16 18:19:31 +0900

    v1.4と同じに

--- Patches/ChatCommandPatch.cs
+++ Patches/ChatCommandPatch.cs
@@ -230,7 +230,7 @@ namespace TownOfHost
                     break;
 
                 default:
-                    main.SendToAll("使用可能な引数(略称): jester(je), madmate(mm), bait(ba), terrorist(te), mafia(mf), vampire(va),\n sabotagemaster(sa), mayor(my), madguardian(mg), madsnitch(msn), opportunist(op), snitch(sn), sheriff(sh), bountyhunter(bo), witch(wi), fox(fo), troll(tr)");
+                    main.SendToAll("使用可能な引数(略称): jester(je), madmate(mm), bait(ba), terrorist(te), mafia(mf), vampire(va),\nsabotagemaster(sa), mayor(my), madguardian(mg), madsnitch(msn), opportunist(op), snitch(sn), sheriff(sh), bountyhunter(bo), witch(wi), fox(fo), troll(tr)");
                     break;
             }
 
