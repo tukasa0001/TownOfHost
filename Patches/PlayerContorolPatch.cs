3df1d02bd102efea8ca1023995f6c41d1a0b0df0
そうくん <daoyecangma@gmail.com>
2022-02-17 00:55:06 +0900

    MOD環境で名前の色が変わらないように変更

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -261,13 +261,13 @@ namespace TownOfHost
                 //名前変更
                 RealName = __instance.getRealName();
 
-                //自分自身の名前の色を変更
+                /*//自分自身の名前の色を変更
                 if(__instance.AmOwner) { //__instanceが自分自身
                     foreach(var pc in PlayerControl.AllPlayerControls) { //全員分ループ
                             RealName = $"<color={__instance.getRoleColorCode()}>{RealName}</color>"; //名前の色を変更
                             break; //無駄なループは行わない
                     }
-                }
+                }*/
 
                 //タスクを終わらせたSnitchがインポスターを確認できる
                 if(PlayerControl.LocalPlayer.isSnitch() && //LocalPlayerがSnitch
