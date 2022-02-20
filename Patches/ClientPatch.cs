3c3f8b90fc721532ed79110966a42db00abb3ba3
tukasa0001 <tts13511@hotmail.com>
2022-02-20 15:10:05 +0900

    連打で公開ルームにできてしまうバグを修正 ついでに公開ルームにしようとしたときの警告も追加

---
+++ Patches/ClientPatch.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnhollowerBaseLib;
+using TownOfHost;
+using Hazel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using InnerNet;
+
+namespace TownOfHost {
+    [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.ChangeGamePublic))]
+    class ChangeGamePublicPatch {
+        public static void Prefix(InnerNetClient __instance, [HarmonyArgument(0)] ref bool isPublic) {
+            if(main.PluginVersionType == VersionTypes.Beta) {
+                if(isPublic) Logger.SendInGame("ベータ版では公開ルームにすることができません。", true);
+                isPublic = false;
+            }
+        }
+    }
+}
