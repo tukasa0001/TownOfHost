kou-hetare/TownOfHost-Core

最終役職の自動表示機能の追加

Patches/GameStartManagerPatch.cs
@@ -39,6 +39,15 @@ namespace TownOfHost
                 // Reset lobby countdown timer
                 timer = 600f;
                 lobbyCodehide = $"<color={main.modColor}>Town Of Host</color>";
+
+                if (AmongUsClient.Instance.AmHost && main.autoDisplayLastRoles && main.lastAllPlayerCustomRoles.Count != 0)
+                {
+                    new LateTask(() =>
+                    {
+                        main.ShowLastRoles();
+                    }
+                        , 5f, "DisplayLastRoles");
+                }
             }
         }
 

tukasa0001/TownOfHost

Patches/GameStartManagerPatch.cs
@@ -45,6 +45,15 @@ namespace TownOfHost
                     __instance.MakePublicButton.color = Palette.DisabledClear;
                     __instance.privatePublicText.color = Palette.DisabledClear;
                 }
+
+                if (AmongUsClient.Instance.AmHost && main.autoDisplayLastRoles && main.AllPlayerCustomRoles.Count != 0)
+                {
+                    new LateTask(() =>
+                    {
+                        main.ShowLastRoles();
+                    }
+                        , 5f, "DisplayLastRoles");
+                }
             }
         }
 