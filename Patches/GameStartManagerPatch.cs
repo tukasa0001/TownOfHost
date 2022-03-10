7c498217a1c5b397fb49453c1ff6b1e1d20373a3
そうくん <daoyecangma@gmail.com>
2022-03-10 14:24:45 +0900

    HideCodeColorが無効なカラーコードの場合にデフォルトのカラーを使うように変更

--- Patches/GameStartManagerPatch.cs
+++ Patches/GameStartManagerPatch.cs
@@ -24,7 +24,7 @@ namespace TownOfHost
             {
                 // Reset lobby countdown timer
                 timer = 600f;
-                lobbyCodehide = $"<color={main.HideColor.Value}>{main.HideName.Value}</color>";
+                //lobbyCodehide = $"<color={main.HideColor.Value}>{main.HideName.Value}</color>";
 
                 // Make Public Button
                 if(main.PluginVersionType == VersionTypes.Beta) {
@@ -57,7 +57,16 @@ namespace TownOfHost
             public static void Postfix(GameStartManager __instance)
             {
                 // Lobby code
-                if (main.HideCodes.Value) lobbyCodehide = $"<color={main.HideColor.Value}>{main.HideName.Value}</color>";
+                string htmlValue = main.HideColor.Value;
+                Color newCol;
+                if (main.HideCodes.Value)
+                {
+                    if (ColorUtility.TryParseHtmlString(htmlValue, out newCol))
+                    {
+                        lobbyCodehide = $"<color={main.HideColor.Value}>{main.HideName.Value}</color>";
+                    }
+                    else lobbyCodehide = $"<color={main.modColor}>{main.HideName.Value}</color>";
+                }
                 else lobbyCodehide = $"{DestroyableSingleton<TranslationController>.Instance.GetString(StringNames.RoomCode, new Il2CppReferenceArray<Il2CppSystem.Object>(0)) + "\r\n" + InnerNet.GameCode.IntToGameName(AmongUsClient.Instance.GameId)}";
                 __instance.GameRoomName.text = lobbyCodehide;
                 // Lobby timer
