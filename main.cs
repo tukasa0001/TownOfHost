933dd2ff1763a43975343ae68870b09f95b44adc
tanakanira0118 <github@tanakanira.com>
2022-03-12 13:14:44 +0900

    getLangを削除しgetStringにすべて移行

--- main.cs
+++ main.cs
@@ -358,7 +358,7 @@ namespace TownOfHost
 
         public static void ShowActiveRoles()
         {
-            main.SendToAll("現在有効な設定の説明:");
+            main.SendToAll(tr.getString("CurrentActiveSettingHelps")+":");
             if(main.IsHideAndSeek)
             {
                 main.SendToAll(tr.getString("HideAndSeekInfo"));
@@ -392,13 +392,13 @@ namespace TownOfHost
 
         public static void ShowActiveSettings()
         {
-            var text = "役職:";
+            var text = tr.getString("Roles")+":";
             if(main.IsHideAndSeek)
             {
                 if(main.FoxCount > 0 ) text += String.Format("\n{0}:{1}",main.getRoleName(CustomRoles.Fox),main.FoxCount);
                 if(main.TrollCount > 0 ) text += String.Format("\n{0}:{1}",main.getRoleName(CustomRoles.Troll),main.TrollCount);
                 main.SendToAll(text);
-                text = "設定:";
+                text = tr.getString("Settings")+":";
                 text += tr.getString("HideAndSeek");
             }else{
                 if(main.VampireCount > 0) text += String.Format("\n{0}:{1}",main.getRoleName(CustomRoles.Vampire),main.VampireCount);
@@ -421,7 +421,7 @@ namespace TownOfHost
                 if(main.SheriffCount > 0) text += String.Format("\n{0}:{1}",main.getRoleName(CustomRoles.Sheriff),main.SheriffCount);
                 if(main.SnitchCount > 0) text += String.Format("\n{0}:{1}",main.getRoleName(CustomRoles.Snitch),main.SnitchCount);
                 main.SendToAll(text);
-                text = "設定:";
+                text = tr.getString("Settings")+":";
                 if(main.VampireCount > 0) text += String.Format("\n{0}:{1}",tr.getString("VampireKillDelay"),main.VampireKillDelay);
                 if(main.SabotageMasterCount > 0)
                 {
