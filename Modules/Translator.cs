ed73b3a17b57e35a0f5ff5ee82cc02cd111095cc
tanakanira0118 <github@tanakanira.com>
2022-03-13 23:42:42 +0900

    Translatorクラスを作成

---
+++ Modules/Translator.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TownOfHost {
+    public static class Translator
+    {
+        public static Dictionary<string, Dictionary<int, string>> tr;
+        static Translator()
+        {
+            Logger.info("Langage Dictionary Initialize...");
+            load();
+            Logger.info("Langage Dictionary Initialize Finished");
+        }
+        public static void load()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream("TownOfHost.Resources.string.csv");
+            var sr = new StreamReader(stream);
+            tr = new Dictionary<string, Dictionary<int, string>>();
+
+            string[] header = sr.ReadLine().Split(',');
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] values = line.Split(',');
+                List<string> fields = new List<string>(values);
+                Dictionary<int, string> tmp = new Dictionary<int, string>();                for(var i=0;i<fields.Count;++i) 
+                {
+                    if(fields[i] != string.Empty && fields[i].TrimStart()[0] == '"')
+                    {
+                        while (fields[i].TrimEnd()[fields[i].TrimEnd().Length - 1] != '"')
+                        {
+                            fields[i] = fields[i] + "," + fields[i + 1];
+                            fields.RemoveAt(i + 1);
+                        }
+                    }
+                }
+                for(var i=1; i < fields.Count; i++)
+                {                    
+                    var tmp_str = fields[i].Replace("\\n","\n").Trim('"');
+                    tmp.Add(Int32.Parse(header[i]),tmp_str);
+                }
+                tr.Add(fields[0],tmp);
+            }
+        }
+
+        public static string getString(string s)
+        {
+            int langId = TranslationController._instance != null ? (int)TranslationController.Instance.CurrentLanguage.languageID:0;
+            var res = "";
+            if(tr.TryGetValue(s,out var dic))
+            {
+                if(dic.TryGetValue(langId,out res))
+                {
+                    return res;
+                } else {
+                    if(dic.TryGetValue(0,out res))
+                    {
+                        return res;
+                    }else{
+                        return $"<INVALID:{s}>";
+                    }
+                }
+            } else {
+                return $"<INVALID:{s}>";
+            }
+        }
+    }
+}
