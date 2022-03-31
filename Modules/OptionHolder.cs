ccf9db437a41c3efb6d56b9ca3ae6587fdacc410
tampopo-dandelion <goukaku2@icloud.com>
2022-03-31 11:20:15 +0900

    confingidの修正

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -247,9 +247,9 @@ namespace TownOfHost
             SheriffShotLimit = CustomOption.Create(20416, Color.white, "SheriffShotLimit", 15, 1, 15, 1, CustomRoleSpawnChances[CustomRoles.Sheriff]);
             SetupRoleOptions(20500, CustomRoles.Snitch);
             // Other
-            SetupRoleOptions(50400, CustomRoles.Arsonist);
-            ArsonistDouseTime = CustomOption.Create(50410, Color.white, "ArsonistDouseTime", 3, 1, 10, 1, CustomRoleSpawnChances[CustomRoles.Arsonist]);
-            ArsonistCooldown = CustomOption.Create(50411, Color.white, "ArsonistCooldown", 10, 5, 100, 1, CustomRoleSpawnChances[CustomRoles.Arsonist]);
+            SetupRoleOptions(50500, CustomRoles.Arsonist);
+            ArsonistDouseTime = CustomOption.Create(50510, Color.white, "ArsonistDouseTime", 3, 1, 10, 1, CustomRoleSpawnChances[CustomRoles.Arsonist]);
+            ArsonistCooldown = CustomOption.Create(50511, Color.white, "ArsonistCooldown", 10, 5, 100, 1, CustomRoleSpawnChances[CustomRoles.Arsonist]);
             SetupRoleOptions(50000, CustomRoles.Jester);
             SetupRoleOptions(50100, CustomRoles.Opportunist);
             SetupRoleOptions(50200, CustomRoles.Terrorist);
