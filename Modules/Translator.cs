42205c1a019cb9326b85422b635cbee71951d494
tanakanira0118 <github@tanakanira.com>
2022-04-01 06:29:24 +0900

    変数名を修正

--- Modules/Translator.cs
+++ Modules/Translator.cs
@@ -49,7 +49,7 @@ namespace TownOfHost
 
         public static string getString(string s)
         {
-            var langId = TranslationController.InstanceExists ? TranslationController.Instance.CurrentLanguage.languageID : SupportedLangs.English;
+            var langId = TranslationController.InstanceExists ? TranslationController.Instance.currentLanguage.languageID : SupportedLangs.English;
             if (main.ForceJapanese.Value) langId = SupportedLangs.Japanese;
             return getString(s, langId);
         }
