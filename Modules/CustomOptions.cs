b3a0aebac96aae3b44621fdcbb4a1ea2d7520dea
tanakanira0118 <github@tanakanira.com>
2022-04-09 12:18:16 +0900

    オンラインでマップを切り替えられるように修正

--- Modules/CustomOptions.cs
+++ Modules/CustomOptions.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnhollowerBaseLib;
 namespace TownOfHost
 {
-    [HarmonyPatch(typeof(GameSettingMenu), nameof(GameSettingMenu.Start))]
+    [HarmonyPatch(typeof(GameSettingMenu), nameof(GameSettingMenu.InitializeOptions))]
     public static class GameSettingMenuPatch
     {
         public static void Prefix(GameSettingMenu __instance)
