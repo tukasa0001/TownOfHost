a26e00a92ea57642fde0d10565b07a3d8fdba0b7
tanakanira0118 <github@tanakanira.com>
2022-04-14 23:53:30 +0900

    getPlayerTaskStateのログを無効化

--- main.cs
+++ main.cs
@@ -938,6 +938,7 @@ namespace TownOfHost
             Logger = BepInEx.Logging.Logger.CreateLogSource("TownOfHost");
             TownOfHost.Logger.enable();
             TownOfHost.Logger.disable("NotifyRoles");
+            TownOfHost.Logger.disable("getPlayerTaskState");
 
             currentWinner = CustomWinner.Default;
             additionalwinners = new HashSet<AdditionalWinners>();
