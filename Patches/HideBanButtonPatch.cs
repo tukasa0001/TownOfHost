a925614869a80d92614a56e718bcce8b605966a6
EmptyBottle <ttes3511@hotmail.com>
2022-04-20 19:08:26 +0900

    チャットを閉じるときにBanButtonも非表示にする処理を追加

---
+++ Patches/HideBanButtonPatch.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using Hazel;
+using HarmonyLib;
+using System.Linq;
+using System;
+
+namespace TownOfHost
+{
+    [HarmonyPatch(typeof(ChatController), nameof(ChatController.Toggle))]
+    class CancelBanMenuStuckPatch
+    {
+        public static void Prefix(ChatController __instance)
+        {
+            if(__instance.IsOpen) // (IsOpen==true) == 今から閉じないといけない
+            {
+                // BanButtonを非表示にする
+                __instance.BanButton.SetVisible(false);
+            }
+        }
+    }
+}
