8d64b98febd53ed300bfa07f92f0d531306a6261
そうくん <daoyecangma@gmail.com>
2022-04-23 02:30:52 +0900

    ロビーでOptionShowerが表示されていないバグを修正

--- Patches/HudPatch.cs
+++ Patches/HudPatch.cs
@@ -40,6 +40,10 @@ namespace TownOfHost
                     player.Collider.offset = new Vector2(0f, -0.3636f);
                 }
             }
+
+            __instance.GameSettings.text = OptionShower.getText();
+            __instance.GameSettings.fontSizeMin =
+            __instance.GameSettings.fontSizeMax = (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Japanese || main.ForceJapanese.Value) ? 1.05f : 1.2f;
             //ゲーム中でなければ以下は実行されない
             if (!AmongUsClient.Instance.IsGameStarted) return;
             //バウンティハンターのターゲットテキスト
@@ -113,10 +117,6 @@ namespace TownOfHost
             }
 
             if (!__instance.TaskText.text.Contains(TaskTextPrefix)) __instance.TaskText.text = TaskTextPrefix + "\r\n" + __instance.TaskText.text;
-
-            __instance.GameSettings.text = OptionShower.getText();
-            __instance.GameSettings.fontSizeMin =
-            __instance.GameSettings.fontSizeMax = (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Japanese || main.ForceJapanese.Value) ? 1.05f : 1.2f;
 
             if (Input.GetKeyDown(KeyCode.Y) && AmongUsClient.Instance.GameMode == GameModes.FreePlay)
             {
