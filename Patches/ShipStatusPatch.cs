0342943955e1c3f21155f08a44416924d28e55a1
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2022-05-07 01:02:01 +0900

    MODクライアント初期化位置変更

--- Patches/ShipStatusPatch.cs
+++ Patches/ShipStatusPatch.cs
@@ -239,7 +239,6 @@ namespace TownOfHost
                 foreach (var pc in PlayerControl.AllPlayerControls)
                 {
                     PlayerState.InitTask(pc);
-                    if (pc.Is(CustomRoles.FireWorks)) FireWorks.Add(pc.PlayerId);
                 }
                 Utils.CountAliveImpostors();
                 Utils.NotifyRoles();
