4b4051c906d0fe16e99061f895b2b9d1f48a57ee
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2022-05-10 21:54:25 +0900

    isTrapper()をIs()に置き換え

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -451,7 +451,7 @@ namespace TownOfHost
                                 __instance.RpcMurderPlayer(bitten);
                                 RPC.PlaySoundRPC(vampireID, Sounds.KillSound);
                                 Logger.SendToFile("Vampireに噛まれている" + bitten.name + "を自爆させました。");
-                                if (bitten.isTrapper())
+                                if (bitten.Is(CustomRoles.Trapper))
                                     Utils.getPlayerById(vampireID).TrapperKilled(bitten);
                             }
                             else
