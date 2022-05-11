9b629109e0a50c2eac0746580314c03a8a90dc65
そうくん <daoyecangma@gmail.com>
2022-05-11 20:18:21 +0900

    ターゲットが死んだときにKeyを削除

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -60,8 +60,14 @@ namespace TownOfHost
                 {
                     var executioner = Utils.getPlayerById(ExecutionerTarget.Key);
                     if (target.PlayerId == ExecutionerTarget.Value && !executioner.Data.IsDead)
+                    {
                         executioner.RpcSetCustomRole(Options.CRoleExecutionerChangeRoles[Options.ExecutionerChangeRolesAfterTargetKilled.GetSelection()]); //対象がキルされたらオプションで設定した役職にする
-                    main.ExecutionerTarget.Remove(ExecutionerTarget.Key);
+                        new LateTask(() =>
+                        {
+                            main.ExecutionerTarget.Remove(ExecutionerTarget.Key);
+                            Utils.NotifyRoles();
+                        }, 0.3f, "Remove Executioner Key");
+                    }
                 }
             }
             foreach (var pc in PlayerControl.AllPlayerControls)
