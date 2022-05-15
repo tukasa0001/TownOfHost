4adc2579e1c79ecbebb2725812c31ac8ce24c69f
shu-TownofHost <itozakishunnya@gmail.com>
2022-05-15 12:38:18 +0900

    コメントをわかりやすく変更

--- Patches/HudPatch.cs
+++ Patches/HudPatch.cs
@@ -40,7 +40,7 @@ namespace TownOfHost
                     player.Collider.offset = new Vector2(0f, -0.3636f);
                 }
             }
-            //MOD入り用のボタンテキスト変更
+            //MOD入り用のボタン下テキスト変更
             switch (player.getCustomRole())
             {
                 case CustomRoles.Sniper:
