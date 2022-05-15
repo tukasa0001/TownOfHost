2dffcee9d5886a1aaeb0f014a0cdb93ea11a8820
tanakanira0118 <github@tanakanira.com>
2022-05-16 03:08:21 +0900

    F11で解像度を変更できるように

--- Patches/ControlPatch.cs
+++ Patches/ControlPatch.cs
@@ -10,12 +10,21 @@ namespace TownOfHost
     {
         static System.Random random = new System.Random();
         static PlayerControl bot;
+        static (int, int)[] resolutions = { (480, 270), (640, 360), (800, 450), (1280, 720), (1600, 900) };
+        static int resolutionIndex = 0;
         public static void Postfix(ControllerManager __instance)
         {
             //カスタム設定切り替え
             if (Input.GetKeyDown(KeyCode.Tab) && GameStates.isLobby)
             {
                 OptionShower.next();
+            }
+            //解像度変更
+            if (Input.GetKeyDown(KeyCode.F11))
+            {
+                resolutionIndex++;
+                if (resolutionIndex >= resolutions.Length) resolutionIndex = 0;
+                ResolutionManager.SetResolution(resolutions[resolutionIndex].Item1, resolutions[resolutionIndex].Item2, false);
             }
 
             //--以下ホスト専用コマンド--//
