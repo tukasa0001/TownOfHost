11c0556f53eb57797b4957d613ba85958b099fde
yurinakira <84663668+yurinakira@users.noreply.github.com>
2022-05-20 20:43:32 +0900

    Delete unnecessary arguments

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -99,7 +99,7 @@ namespace TownOfHost
     [HarmonyPatch(typeof(RoleManager), nameof(RoleManager.SelectRoles))]
     class SelectRolesPatch
     {
-        public static void Prefix(RoleManager __instance)
+        public static void Prefix()
         {
             if (!AmongUsClient.Instance.AmHost) return;
 
@@ -198,7 +198,7 @@ namespace TownOfHost
                 }
             }
         }
-        public static void Postfix(RoleManager __instance)
+        public static void Postfix()
         {
             if (!AmongUsClient.Instance.AmHost) return;
             //Utils.ApplySuffix();
@@ -357,7 +357,7 @@ namespace TownOfHost
                     if (pc.Data.Role.Role == RoleTypes.Shapeshifter) Main.CheckShapeshift.Add(pc.PlayerId, false);
                     if (pc.Is(CustomRoles.Arsonist))
                     {
-                        var targetPlayerCount = (PlayerControl.AllPlayerControls.Count - 1);
+                        var targetPlayerCount = PlayerControl.AllPlayerControls.Count - 1;
                         Main.DousedPlayerCount[pc.PlayerId] = (0, targetPlayerCount);
                         pc.RpcSendDousedPlayerCount();
                         foreach (var ar in PlayerControl.AllPlayerControls)
