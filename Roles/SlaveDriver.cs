c5d8cc6cfcef68e6f1bd7a78fdb69130438211e0
shu-TownofHost <itozakishunnya@gmail.com>
2022-05-21 13:18:12 +0900

    ログを追加

--- Roles/SlaveDriver.cs
+++ Roles/SlaveDriver.cs
@@ -23,14 +23,17 @@ namespace TownOfHost
 
             if (taskState.CompletedTasksCount <= TaskHalfValue)//キル対象の完了タスク数が設定タスク数の半分か、それ以下
             {
+                Logger.Info($"SlaveDriver Kill 1", "SlaveDriver");
                 Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() * 1.5f;
             }
             if (taskState.CompletedTasksCount > TaskHalfValue)//キル対象の完了タスク数が設定タスク数の半分を超えている
             {
+                Logger.Info($"SlaveDriver Kill 2", "SlaveDriver");
                 Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() / 1.4f;
             }
             if (taskState.IsTaskFinished)//キル対象がタスクを終えている
             {
+                Logger.Info($"SlaveDriver Kill 3", "SlaveDriver");
                 Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() / 1.9f;
             }
         }
