0548534bfcea1071476b3e2e0a4c25bfea9229ce
shu-TownofHost <itozakishunnya@gmail.com>
2022-05-25 17:43:11 +0900

    __instanceの記述をkillerに変更

--- Roles/SlaveDriver.cs
+++ Roles/SlaveDriver.cs
@@ -15,28 +15,28 @@ namespace TownOfHost
         {
             Options.SetupRoleOptions(Id, CustomRoles.SlaveDriver);
         }
-        public static void SlaveDriverKillTargetTaskCheck(PlayerControl __instance, byte playerId)
+        public static void SlaveDriverKillTargetTaskCheck(PlayerControl killer, byte playerId)
         {
             var taskState = PlayerState.taskState?[playerId];
             int TaskHalfValue = taskState.AllTasksCount / 2;
             if (taskState.CompletedTasksCount <= TaskHalfValue)//キル対象の完了タスク数が設定タスク数の半分か、それ以下
             {
                 Logger.Info($"SlaveDriver Kill 1", "SlaveDriver");
-                Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() * 1.5f;
-                __instance.CustomSyncSettings();//負荷軽減&ホストのキルクール変更を反映させるため、__instanceだけがCustomSyncSettingsを実行
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() * 1.5f;
+                killer.CustomSyncSettings();//負荷軽減するため、__instanceだけがCustomSyncSettingsを実行
             }
             if (taskState.CompletedTasksCount > TaskHalfValue)//キル対象の完了タスク数が設定タスク数の半分を超えている
             {
                 Logger.Info($"SlaveDriver Kill 2", "SlaveDriver");
-                Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() / 1.4f;
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() / 1.4f;
             }
             if (taskState.IsTaskFinished)//キル対象がタスクを終えている
             {
                 Logger.Info($"SlaveDriver Kill 3", "SlaveDriver");
-                Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() / 2f;
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.BHDefaultKillCooldown.GetFloat() / 2f;
             }
             if (taskState.hasTasks == false)
-                Main.AllPlayerKillCooldown[__instance.PlayerId] = Options.BHDefaultKillCooldown.GetFloat();
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.BHDefaultKillCooldown.GetFloat();
         }
     }
 }
