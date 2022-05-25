96e2f25bf9bec8bbdfe591f58566ad72e6fd01a9
shu-TownofHost <itozakishunnya@gmail.com>
2022-05-25 19:16:59 +0900

    不要なusingを削除

--- Roles/SlaveDriver.cs
+++ Roles/SlaveDriver.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HarmonyLib;
-using Hazel;
-
 namespace TownOfHost
 {
     public static class SlaveDriver
