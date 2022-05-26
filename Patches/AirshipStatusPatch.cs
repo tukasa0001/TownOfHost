85fa96fa6d24a6abf4bd76971a04a993861ae1b1
そうくん <76250388+Palpunte-Soukun@users.noreply.github.com>
2022-05-26 17:11:44 +0900

    MOD環境で、ReSpawnStepを無効化

---
+++ Patches/AirshipStatusPatch.cs
@@ -0,0 +1,14 @@
+using HarmonyLib;
+
+namespace TownOfHost
+{
+    //元:https://github.com/yukieiji/ExtremeRoles/blob/master/ExtremeRoles/Patches/AirShipStatusPatch.cs
+    [HarmonyPatch(typeof(AirshipStatus), nameof(AirshipStatus.PrespawnStep))]
+    public static class AirshipStatusPrespawnStepPatch
+    {
+        public static bool Prefix()
+        {
+            return !Assassin.IsAssassinMeeting;
+        }
+    }
+}
