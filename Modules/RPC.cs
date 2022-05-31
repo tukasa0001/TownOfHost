44fd2c3a0bee4d54922008febb4df4bda6e13d97
yurinakira <84663668+yurinakira@users.noreply.github.com>
2022-06-01 00:14:21 +0900

    ref move sniper init

--- Modules/RPC.cs
+++ Modules/RPC.cs
@@ -329,13 +329,13 @@ namespace TownOfHost
             if (role < CustomRoles.NoSubRoleAssigned)
             {
                 Main.AllPlayerCustomRoles[targetId] = role;
-                if (role == CustomRoles.Sniper) Sniper.Add(targetId);
             }
             else if (role >= CustomRoles.NoSubRoleAssigned)   //500:NoSubRole 501~:SubRole
             {
                 Main.AllPlayerCustomSubRoles[targetId] = role;
             }
             if (role == CustomRoles.FireWorks) FireWorks.Add(targetId);
+            if (role == CustomRoles.Sniper) Sniper.Add(targetId);
             HudManager.Instance.SetHudActive(true);
         }
         public static void AddNameColorData(byte seerId, byte targetId, string color)
