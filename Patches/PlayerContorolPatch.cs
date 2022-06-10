81def704f386d7401b90d713878cf40707fb0577
tanakanira0118 <github@tanakanira.com>
2022-06-10 22:01:23 +0900

    ミーティング中ならキャンセル

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -478,6 +478,7 @@ namespace TownOfHost
     {
         public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] GameData.PlayerInfo target)
         {
+            if (GameStates.IsMeeting) return false;
             Logger.Info($"{__instance.GetNameWithRole()} => {target?.GetNameWithRole() ?? "null"}", "ReportDeadBody");
             if (Options.StandardHAS.GetBool() && target != null && __instance == target.Object) return true; //[StandardHAS] ボタンでなく、通報者と死体が同じなら許可
             if (Options.CurrentGameMode == CustomGameMode.HideAndSeek || Options.StandardHAS.GetBool()) return false;
