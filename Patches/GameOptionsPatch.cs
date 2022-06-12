2c9ab6d9a87670436af535b95b2649511a6807a9
そうくん <76250388+Palpunte-Soukun@users.noreply.github.com>
2022-06-12 14:03:12 +0900

    一応守護天使にも適応

--- Patches/GameOptionsPatch.cs
+++ Patches/GameOptionsPatch.cs
@@ -18,6 +18,10 @@ namespace TownOfHost
                 __instance.TitleText.color = Utils.GetRoleColor(CustomRoles.Engineer);
                 if (CustomRoles.Madmate.IsEnable() || CustomRoles.Terrorist.IsEnable()) forced = true;
             }
+            if (__instance.Role.Role == RoleTypes.GuardianAngel)
+            {
+                __instance.TitleText.color = Utils.GetRoleColor(CustomRoles.GuardianAngel);
+            }
             if (__instance.Role.Role == RoleTypes.Shapeshifter)
             {
                 __instance.TitleText.color = Utils.GetRoleColor(CustomRoles.Shapeshifter);
