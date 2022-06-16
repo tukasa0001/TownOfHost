c99db1e431a359dc6eda35dcf6c1400830c6a2d2
EmptyBottle <ttes3511@hotmail.com>
2022-06-17 00:23:22 +0900

    CustomRpcSenderExtentionsでAutoStartRpcを使うよう変更

--- Modules/CustomRpcSender.cs
+++ Modules/CustomRpcSender.cs
@@ -255,15 +255,15 @@ namespace TownOfHost
 
     public static class CustomRpcSenderExtentions
     {
-        public static void RpcSetRole(this CustomRpcSender sender, PlayerControl player, RoleTypes role)
+        public static void RpcSetRole(this CustomRpcSender sender, PlayerControl player, RoleTypes role, int targetClientId = -1)
         {
-            sender.StartRpc(player.NetId, (byte)RpcCalls.SetRole)
+            sender.AutoStartRpc(player.NetId, (byte)RpcCalls.SetRole, targetClientId)
               .Write((ushort)role)
               .EndRpc();
         }
-        public static void RpcMurderPlayer(this CustomRpcSender sender, PlayerControl player, PlayerControl target)
+        public static void RpcMurderPlayer(this CustomRpcSender sender, PlayerControl player, PlayerControl target, int targetClientId = -1)
         {
-            sender.StartRpc(player.NetId, (byte)RpcCalls.MurderPlayer)
+            sender.AutoStartRpc(player.NetId, (byte)RpcCalls.MurderPlayer, targetClientId)
               .WriteNetObject(target)
               .EndRpc();
         }
