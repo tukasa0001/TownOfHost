ddfb1aaa3a22edc070cf52a8dfd241eeec14a98b
そうくん <76250388+Palpunte-Soukun@users.noreply.github.com>
2022-06-20 23:05:47 +0900

    キルクールを最新のものに

--- Roles/SlaveDriver.cs
+++ Roles/SlaveDriver.cs
@@ -40,17 +40,17 @@ namespace TownOfHost
             if (taskState.CompletedTasksCount <= TaskHalfValue)//キル対象の完了タスク数が設定タスク数の半分か、それ以下
             {
                 Logger.Info($"SlaveDriver Kill 1", "SlaveDriver");
-                Main.AllPlayerKillCooldown[killer.PlayerId] = Main.RealOptionsData.killCooldown * SlaveDriverIncreaseKC.GetFloat();
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.DefaultKillCooldown * SlaveDriverIncreaseKC.GetFloat();
             }
             if (taskState.CompletedTasksCount > TaskHalfValue)//キル対象の完了タスク数が設定タスク数の半分を超えている
             {
                 Logger.Info($"SlaveDriver Kill 2", "SlaveDriver");
-                Main.AllPlayerKillCooldown[killer.PlayerId] = Main.RealOptionsData.killCooldown / SlaveDriverDecreaseKC.GetFloat();
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.DefaultKillCooldown / SlaveDriverDecreaseKC.GetFloat();
             }
             if (taskState.IsTaskFinished)//キル対象がタスクを終えている
             {
                 Logger.Info($"SlaveDriver Kill 3", "SlaveDriver");
-                Main.AllPlayerKillCooldown[killer.PlayerId] = Main.RealOptionsData.killCooldown / SlaveDriverTaskCompleteDecreaseKC.GetFloat();
+                Main.AllPlayerKillCooldown[killer.PlayerId] = Options.DefaultKillCooldown / SlaveDriverTaskCompleteDecreaseKC.GetFloat();
             }
             if (taskState.hasTasks == false)//キル対象のタスクがない
                 Main.AllPlayerKillCooldown[killer.PlayerId] = SlaveDriverNoTaskKC.GetFloat();
