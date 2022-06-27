96697d554b1644eb19ca820d3d30789f88f8be41
そうくん <76250388+Palpunte-Soukun@users.noreply.github.com>
2022-06-27 12:19:49 +0900

    MODクライアントの仕様を変更

--- Patches/Devices/Admin.cs
+++ Patches/Devices/Admin.cs
@@ -12,10 +12,10 @@ namespace TownOfHost
         public static bool DisableArchiveAdmin => Options.DisableAdmin.GetBool() && PlayerControl.GameOptions.MapId == 4 && Options.WhichDisableAdmin.GetString() == GetString(Options.whichDisableAdmin[1]);
         public static Vector2 ArchiveAdminPos = new(20.0f, 12.3f);
         public static Vector2 SecondaryPolusAdminPos = new(24.66107f, -21.523f);
-        [HarmonyPatch(typeof(MapConsole), nameof(MapConsole.Use))]
-        public static class MapConsoleUsePatch
+        [HarmonyPatch(typeof(MapCountOverlay), nameof(MapCountOverlay.Update))]
+        public static class MapCountOverlayUpdatePatch
         {
-            public static bool Prefix()
+            public static bool Prefix(MapCountOverlay __instance)
             {
                 bool isGuard = false;
                 if (DisableAdmin)
@@ -39,8 +39,15 @@ namespace TownOfHost
                         isGuard = ArchiveAdminDistance <= DisableDevice.UsableDistance();
                     }
                 }
+                if (isGuard)
+                {
+                    __instance.BackgroundColor.SetColor(Palette.DisabledGrey);
+                    __instance.SabotageText.gameObject.SetActive(true);
+                    __instance.SabotageText.text = GetString("DisabledBySettings");
+                    return false;
+                }
 
-                return !isGuard;
+                return true;
             }
         }
     }
