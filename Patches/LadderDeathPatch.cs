083a4aacac1eaa57a6905a24a1e869e540cbae0a
ykundesu <4476tv@gmail.com>
2022-07-01 20:22:30 +0900

    翻訳などを変更

--- Patches/LadderDeathPatch.cs
+++ Patches/LadderDeathPatch.cs
@@ -8,17 +8,17 @@ namespace TownOfHost
 {
     public class LadderDeathPatch
     {
-        public static Dictionary<byte, Vector3> TargetLadderDatas;
+        public static Dictionary<byte, Vector3> TargetLadderData;
         public static void Reset()
         {
-            TargetLadderDatas = new();
+            TargetLadderData = new();
         }
         public static void FixedUpdate(PlayerControl player)
         {
             if (player.Data.Disconnected) return;
-            if (TargetLadderDatas.ContainsKey(player.PlayerId))
+            if (TargetLadderData.ContainsKey(player.PlayerId))
             {
-                if (Vector2.Distance(TargetLadderDatas[player.PlayerId], player.transform.position) < 0.5f)
+                if (Vector2.Distance(TargetLadderData[player.PlayerId], player.transform.position) < 0.5f)
                 {
                     if (player.Data.IsDead) return;
                     //LateTaskを入れるため、先に死亡判定を入れておく
@@ -46,7 +46,7 @@ namespace TownOfHost
                     int chance = UnityEngine.Random.Range(1, 10);
                     if (Chance <= chance)
                     {
-                        TargetLadderDatas[__instance.myPlayer.PlayerId] = targetpos;
+                        TargetLadderData[__instance.myPlayer.PlayerId] = targetpos;
                     }
                 }
             }
