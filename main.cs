473d482132a13c5e92f6ffb1261ca21c9fe6e065
そうくん <76250388+Palpunte-Soukun@users.noreply.github.com>
2022-07-02 13:32:07 +0900

    2.2.0にアップデート

--- main.cs
+++ main.cs
@@ -17,7 +17,7 @@ namespace TownOfHost
     {
         //Sorry for many Japanese comments.
         public const string PluginGuid = "com.emptybottle.townofhost";
-        public const string PluginVersion = "2.1.1";
+        public const string PluginVersion = "2.2.0";
         public Harmony Harmony { get; } = new Harmony(PluginGuid);
         public static Version version = Version.Parse(PluginVersion);
         public static BepInEx.Logging.ManualLogSource Logger;
