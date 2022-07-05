d5a48026d8a807c7f156f187faddcf9b2c99329a
そうくん <daoyecangma@gmail.com>
2022-07-05 17:29:34 +0900

    タイムシーフが心中した後に会議時間を戻すように

--- Patches/ExilePatch.cs
+++ Patches/ExilePatch.cs
@@ -90,6 +90,8 @@ namespace TownOfHost
                 PlayerState.SetDeathReason(x.Key, x.Value);
                 PlayerState.SetDead(x.Key);
                 player?.RpcExileV2();
+                if (player.Is(CustomRoles.TimeThief) && x.Value == PlayerState.DeathReason.LoversSuicide)
+                    player?.ResetThiefVotingTime();
             });
             Main.AfterMeetingDeathPlayers.Clear();
             LadderDeathPatch.Reset();
