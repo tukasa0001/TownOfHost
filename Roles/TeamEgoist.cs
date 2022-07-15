8eedb69ddcb89c2c8eaff0e43c79d40009e10c1b
そうくん <daoyecangma@gmail.com>
2022-07-15 17:42:03 +0900

    ラバーズが勝てないように

--- Roles/TeamEgoist.cs
+++ Roles/TeamEgoist.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TownOfHost
 {
@@ -10,6 +11,7 @@ namespace TownOfHost
         {
             playerIdList.Add(teamEgo);
         }
+        public static bool CompleteWinCondition(byte id) => Main.currentWinner == CustomWinner.Impostor && !PlayerState.isDead[id] && !PlayerControl.AllPlayerControls.ToArray().Any(p => p.Is(RoleType.Impostor) && !PlayerState.isDead[p.PlayerId]);
         public static void SoloWin(List<PlayerControl> winner)
         {
             if (Main.currentWinner == CustomWinner.Egoist && CustomRoles.Egoist.IsEnable()) //横取り勝利
@@ -18,8 +20,10 @@ namespace TownOfHost
                 foreach (var id in playerIdList)
                 {
                     var teamEgo = Utils.GetPlayerById(id);
-                    if (teamEgo != null && ((teamEgo.Is(CustomRoles.Egoist) && !PlayerState.isDead[id]) ||
-                        teamEgo.Is(CustomRoles.EgoSchrodingerCat)))
+                    if (teamEgo == null) continue;
+                    if (teamEgo.Is(CustomRoles.Lovers)) continue; //リア充は無視
+                    else if ((teamEgo.Is(CustomRoles.Egoist) && !PlayerState.isDead[id]) ||
+                        teamEgo.Is(CustomRoles.EgoSchrodingerCat))
                     {
                         winner.Add(teamEgo);
                     }
