290de502b3486ba8fe0b4749dbc556e37c9f8eb8
Masser <103577031+Masami4711@users.noreply.github.com>
2022-08-19 12:11:29 +0900

    条件を修正

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -944,7 +944,7 @@ namespace TownOfHost
                             }
                         }
                     }
-                    if (!GameStates.IsMeeting && target.Is(CustomRoles.EvilTracker)) Suffix += EvilTracker.PCGetTargetArrow(seer, target);
+                    if (GameStates.IsInTask && target.Is(CustomRoles.EvilTracker)) Suffix += EvilTracker.PCGetTargetArrow(seer, target);
 
                     /*if(main.AmDebugger.Value && main.BlockKilling.TryGetValue(target.PlayerId, out var isBlocked)) {
                         Mark = isBlocked ? "(true)" : "(false)";
