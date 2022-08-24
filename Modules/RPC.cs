4bc0e3c909a6411ef481a79c25b648ee74b5551a
EmptyBottle <ttes3511@hotmail.com>
2022-08-24 22:54:11 +0900

    受信したforkIdを使っていなかった問題を修正

--- Modules/RPC.cs
+++ Modules/RPC.cs
@@ -86,7 +86,7 @@ namespace TownOfHost
                         Version version = Version.Parse(reader.ReadString());
                         string tag = reader.ReadString();
                         string forkId = 3 <= version.Major ? reader.ReadString() : Main.OriginalForkId;
-                        Main.playerVersion[__instance.PlayerId] = new PlayerVersion(version, tag, Main.ForkId);
+                        Main.playerVersion[__instance.PlayerId] = new PlayerVersion(version, tag, forkId);
                     }
                     catch
                     {
