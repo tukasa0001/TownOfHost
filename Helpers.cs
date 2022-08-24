ac7380f6ec46c930f04ea2cb86470d2d2976c932
EmptyBottle <ttes3511@hotmail.com>
2022-08-24 23:17:59 +0900

    コンストラクタを修正

--- Helpers.cs
+++ Helpers.cs
@@ -73,9 +73,9 @@ namespace TownOfHost
         public readonly Version version;
         public readonly string tag;
         public readonly string forkId;
-        [Obsolete] public PlayerVersion(string ver, string tag_str) => new PlayerVersion(Version.Parse(ver), tag_str, "");
-        [Obsolete] public PlayerVersion(Version ver, string tag_str) => new PlayerVersion(ver, tag_str, "");
-        public PlayerVersion(string ver, string tag_str, string forkId) => new PlayerVersion(Version.Parse(ver), tag_str, forkId);
+        [Obsolete] public PlayerVersion(string ver, string tag_str) : this(Version.Parse(ver), tag_str, "") { }
+        [Obsolete] public PlayerVersion(Version ver, string tag_str) : this(ver, tag_str, "") { }
+        public PlayerVersion(string ver, string tag_str, string forkId) : this(Version.Parse(ver), tag_str, forkId) { }
         public PlayerVersion(Version ver, string tag_str, string forkId)
         {
             version = ver;
