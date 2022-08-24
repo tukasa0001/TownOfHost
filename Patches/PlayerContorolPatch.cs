b12bba1a4d567bc517ad865bd88ea6d8c09d3134
EmptyBottle <ttes3511@hotmail.com>
2022-08-24 22:51:57 +0900

    フォークIDの表示にvがついているのを修正

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -751,7 +751,7 @@ namespace TownOfHost
                     if (Main.playerVersion.TryGetValue(__instance.PlayerId, out var ver))
                     {
                         if (Main.ForkId != ver.forkId) // フォークIDが違う場合
-                            __instance.cosmetics.nameText.text = $"<color=#ff0000><size=1.2>v{ver.forkId}</size>\n{__instance?.name}</color>";
+                            __instance.cosmetics.nameText.text = $"<color=#ff0000><size=1.2>{ver.forkId}</size>\n{__instance?.name}</color>";
                         else if (Main.version.CompareTo(ver.version) == 0)
                             __instance.cosmetics.nameText.text = ver.tag == $"{ThisAssembly.Git.Commit}({ThisAssembly.Git.Branch})" ? $"<color=#87cefa>{__instance.name}</color>" : $"<color=#ffff00><size=1.2>{ver.tag}</size>\n{__instance?.name}</color>";
                         else __instance.cosmetics.nameText.text = $"<color=#ff0000><size=1.2>v{ver.version}</size>\n{__instance?.name}</color>";
