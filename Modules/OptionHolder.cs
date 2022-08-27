c31b5781f9c6b00612e2236ed1ddc7fb531de129
そうくん <daoyecangma@gmail.com>
2022-08-27 20:33:07 +0900

    設定追加

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -182,6 +182,7 @@ namespace TownOfHost
         public static CustomOption GhostCanSeeOtherRoles;
         public static CustomOption GhostCanSeeOtherVotes;
         public static CustomOption HideGameSettings;
+        public static CustomOption RandomSpawn;
         public static readonly string[] suffixModes =
         {
             "SuffixMode.None",
@@ -455,6 +456,8 @@ namespace TownOfHost
             GhostCanSeeOtherRoles = CustomOption.Create(100603, Color.white, "GhostCanSeeOtherRoles", true)
                 .SetGameMode(CustomGameMode.All);
             GhostCanSeeOtherVotes = CustomOption.Create(100604, Color.white, "GhostCanSeeOtherVotes", true)
+                .SetGameMode(CustomGameMode.All);
+            RandomSpawn = CustomOption.Create(100607, Color.white, "RandomSpawn", false)
                 .SetGameMode(CustomGameMode.All);
             HideGameSettings = CustomOption.Create(100606, Color.white, "HideGameSettings", false)
                 .SetGameMode(CustomGameMode.All);
