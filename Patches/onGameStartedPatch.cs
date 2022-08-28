39908c9e2ae221d9d673e8dafb58ba53a48bba63
そうくん <daoyecangma@gmail.com>
2022-08-28 23:01:45 +0900

    onGameStartedPatchで一斉にDictionaryに登録するように

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -77,6 +77,8 @@ namespace TownOfHost
                 Main.PlayerColors[pc.PlayerId] = Palette.PlayerColors[pc.Data.DefaultOutfit.ColorId];
                 Main.AllPlayerSpeed[pc.PlayerId] = Main.RealOptionsData.PlayerSpeedMod; //移動速度をデフォルトの移動速度に変更
                 pc.cosmetics.nameText.text = pc.name;
+
+                RandomSpawnPatch.NumOfTP.Add(pc.PlayerId, 0);
             }
             Main.VisibleTasksCount = true;
             if (__instance.AmHost)
