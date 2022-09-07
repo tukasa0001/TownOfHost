584902b30dfc28e6ea7a654b0554dcffb8ae5e3b
EmptyBottle <ttes3511@hotmail.com>
2022-09-07 20:27:14 +0900

    AdditionalWinners型に変更

--- Modules/CustomWinnerHolder.cs
+++ Modules/CustomWinnerHolder.cs
@@ -13,7 +13,7 @@ namespace TownOfHost
         public static CustomWinner WinnerTeam;
         // 追加勝利するプレイヤーのチームが格納されます。
         // リザルトの表示に使用されます。
-        public static HashSet<CustomWinner> AdditionalWinnerTeams;
+        public static HashSet<AdditionalWinners> AdditionalWinnerTeams;
         // 勝者の役職が格納され、この変数に格納されている役職のプレイヤーは全員勝利となります。
         // チームとなる第三陣営の処理に最適です。
         public static HashSet<CustomRoles> WinnerRoles;
@@ -54,7 +54,7 @@ namespace TownOfHost
             AdditionalWinnerTeams = new();
             int AdditionalWinnerTeamsCount = reader.ReadInt32();
             for (int i = 0; i < AdditionalWinnerTeamsCount; i++)
-                AdditionalWinnerTeams.Add((CustomWinner)reader.ReadInt32());
+                AdditionalWinnerTeams.Add((AdditionalWinners)reader.ReadInt32());
 
             WinnerRoles = new();
             int WinnerRolesCount = reader.ReadInt32();
