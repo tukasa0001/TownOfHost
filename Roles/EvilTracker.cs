8cd45257f581f20eb757157f4c2c275191f7d20f
Masser <103577031+Masami4711@users.noreply.github.com>
2022-09-10 11:16:42 +0900

    矢印の対象が切断でエラーになる問題を修正

--- Roles/EvilTracker.cs
+++ Roles/EvilTracker.cs
@@ -129,7 +129,7 @@ namespace TownOfHost
             {
                 var target = Utils.GetPlayerById(arrow.Key.Item2);
                 bool EvilTrackerTarget = seer.GetTarget() == target;
-                if (arrow.Key.Item1 == seer.PlayerId && !target.Data.IsDead && (target.GetCustomRole().IsImpostor() || EvilTrackerTarget))
+                if (arrow.Key.Item1 == seer.PlayerId && !PlayerState.isDead[arrow.Key.Item2] && (target.GetCustomRole().IsImpostor() || EvilTrackerTarget))
                     SelfSuffix += EvilTrackerTarget ? Helpers.ColorString(Utils.GetRoleColor(CustomRoles.Crewmate), arrow.Value) : arrow.Value;
             }
             return SelfSuffix;
