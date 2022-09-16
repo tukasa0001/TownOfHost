c1671e647845687941e9814ca7c8dfd372ce8af1
tanakanira0118 <github@tanakanira.com>
2022-09-16 22:57:50 +0900

    キルフラッシュの条件にマッドメイトを追加

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -102,6 +102,7 @@ namespace TownOfHost
                 case CustomRoles.Seer:
                     return true;
                 default:
+                    if (seer.GetCustomRole().IsMadmate() && Options.MadmateCanSeeKillFlash.GetBool()) return true;
                     return false;
             }
         }
