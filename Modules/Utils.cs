d3c70a52e1dd77a8286ecb2b72994885fecc2607
schwKatz <katz.schw@gmail.com>
2022-09-16 19:52:47 +0900

    役職 クライアント 基礎部分追加

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -275,6 +275,7 @@ namespace TownOfHost
                     if (cRole == CustomRoles.JSchrodingerCat) hasTasks = false;
                     if (cRole == CustomRoles.Egoist) hasTasks = false;
                     if (cRole == CustomRoles.Jackal) hasTasks = false;
+                    if (cRole == CustomRoles.JClient) hasTasks = false;
                 }
                 var cSubRoleFound = Main.AllPlayerCustomSubRoles.TryGetValue(p.PlayerId, out var cSubRole);
                 if (cSubRoleFound)
