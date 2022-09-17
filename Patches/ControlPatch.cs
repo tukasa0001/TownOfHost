66648e7c17abd103b278250c8366199ffa42ec31
tanakanira0118 <github@tanakanira.com>
2022-09-18 03:49:42 +0900

    解像度に1920x1080を追加

--- Patches/ControlPatch.cs
+++ Patches/ControlPatch.cs
@@ -10,7 +10,7 @@ namespace TownOfHost
     {
         static readonly System.Random random = new();
         static PlayerControl bot;
-        static readonly (int, int)[] resolutions = { (480, 270), (640, 360), (800, 450), (1280, 720), (1600, 900) };
+        static readonly (int, int)[] resolutions = { (480, 270), (640, 360), (800, 450), (1280, 720), (1600, 900), (1920, 1080) };
         static int resolutionIndex = 0;
         public static void Postfix(ControllerManager __instance)
         {
