36a08283330c56d5867c0572565990cf7c49522b
tanakanira0118 <github@tanakanira.com>
2022-09-23 06:59:33 +0900

    その他の設定の表示をメソッドに置き換え

--- Modules/OptionShower.cs
+++ Modules/OptionShower.cs
@@ -85,54 +85,16 @@ namespace TownOfHost
                         text += $"\t{Options.CanMakeMadmateCount.GetName()}: {Options.CanMakeMadmateCount.GetString()}\n";
                     }
                     if ((kvp.Key == CustomRoles.EvilTracker && EvilTracker.CanSeeKillFlash.GetBool())
-                    || kvp.Key == CustomRoles.Seer
-                    || (kvp.Key.IsMadmate() && Options.MadmateCanSeeKillFlash.GetBool()))
-                    {
-                        text += $"\t{Options.KillFlashDuration.GetName()}: {Options.KillFlashDuration.GetString()}\n";
-                    }
-                    text += "\n";
+
+                foreach (var opt in CustomOption.Options.Where(x => x.Id >= 90000 && !x.IsHidden(Options.CurrentGameMode) && x.Parent == null))
+                {
+                    if (opt.isHeader) text += "\n";
+                    text += $"{opt.GetName()}: {opt.GetString()}\n";
+                    if (opt.Enabled)
+                        ShowChildren(opt, ref text, 1);
                 }
                 //Onの時に子要素まで表示するメソッド
-                void listUp(CustomOption o)
-                {
-                    if (o.GetBool())
-                    {
-                        text += $"{o.GetName()}: {o.GetString()}\n";
-                        foreach (var c in o.Children)
-                        {
-                            text += $"\t{c.GetName_v()}: {c.GetString()}\n";
-                            if (c.Children != null)
-                                foreach (var c2 in c.Children)
-                                    text += $"\t\t{c2.GetName_v()}: {c2.GetString()}\n";
-                        }
-                        text += "\n";
-                    }
-                }
                 void nameAndValue(CustomOption o) => text += $"{o.GetName()}: {o.GetString()}\n";
-                if (Options.CurrentGameMode == CustomGameMode.Standard)
-                {
-                    listUp(Options.SyncButtonMode);
-                    listUp(Options.VoteMode);
-                    listUp(Options.SabotageTimeControl);
-                    nameAndValue(Options.StandardHAS);
-                }
-                else if (Options.CurrentGameMode == CustomGameMode.HideAndSeek)
-                {
-                    nameAndValue(Options.AllowCloseDoors);
-                    nameAndValue(Options.KillDelay);
-                    //nameAndValue(Options.IgnoreCosmetics);
-                    nameAndValue(Options.IgnoreVent);
-                }
-                text += "\n";
-                listUp(Options.AllAliveMeeting);
-                listUp(Options.LadderDeath);
-                listUp(Options.DisableTasks);
-                listUp(Options.RandomMapsMode);
-                listUp(Options.DisableDevices);
-                nameAndValue(Options.NoGameEnd);
-                nameAndValue(Options.GhostCanSeeOtherRoles);
-                nameAndValue(Options.HideGameSettings);
-                listUp(Options.RandomSpawn);
             }
             //1ページにつき35行までにする処理
             List<string> tmp = new(text.Split("\n\n"));
