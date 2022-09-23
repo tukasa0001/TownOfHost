dbfeac31dd1bdc367ecf8ac63e136981f393ea0b
tanakanira0118 <github@tanakanira.com>
2022-09-23 20:37:40 +0900

    バージョンを3.0.1に変更

--- main.cs
+++ main.cs
@@ -34,7 +34,7 @@ namespace TownOfHost
         public const string OriginalForkId = "OriginalTOH"; // Don't Change The Value. / この値を変更しないでください。
         //Sorry for many Japanese comments.
         public const string PluginGuid = "com.emptybottle.townofhost";
-        public const string PluginVersion = "3.0.0";
+        public const string PluginVersion = "3.0.1";
         public Harmony Harmony { get; } = new Harmony(PluginGuid);
         public static Version version = Version.Parse(PluginVersion);
         public static BepInEx.Logging.ManualLogSource Logger;
