666634a793c3edb993ff16fcef7bf1804dafde20
tukasa0001 <ttes3511@hotmail.com>
2022-09-25 15:41:37 +0900

    オブジェクト生成処理を追加

--- Modules/ErrorText.cs
+++ Modules/ErrorText.cs
@@ -4,6 +4,19 @@ namespace TownOfHost
 {
     public class ErrorText : DestroyableSingleton<ErrorText>
     {
+        public static void Create(TMPro.TextMeshPro baseText)
+        {
+            var Text = Instantiate(baseText);
+            var instance = Text.gameObject.AddComponent<ErrorText>();
+            instance.Text = Text;
+            DontDestroyOnLoad(instance.gameObject);
+            instance.DontDestroy = true;
 
+            Text.enabled = false;
+            Text.text = "";
+            Text.alignment = TMPro.TextAlignmentOptions.Top;
+        }
+
+        public TMPro.TextMeshPro Text;
     }
 }
