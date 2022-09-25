aa7968b913f36f2f79ff58a8632537a9ae4c04c4
tukasa0001 <ttes3511@hotmail.com>
2022-09-25 15:51:34 +0900

    テキストの位置を変更する処理を追加

--- Modules/ErrorText.cs
+++ Modules/ErrorText.cs
@@ -10,13 +10,27 @@ namespace TownOfHost
             var instance = Text.gameObject.AddComponent<ErrorText>();
             instance.Text = Text;
             DontDestroyOnLoad(instance.gameObject);
-            instance.DontDestroy = true;
+            //instance.DontDestroy = true;
 
-            Text.enabled = false;
-            Text.text = "";
+            Text.enabled = true;
+            Text.text = "TestMessage";
             Text.alignment = TMPro.TextAlignmentOptions.Top;
+            Text.transform.localPosition = Vector3.zero;
         }
 
         public TMPro.TextMeshPro Text;
+        public Camera Camera;
+        public Vector3 TextOffset = new(0, 0.3f, -1000f);
+        public void LateUpdate()
+        {
+            if (!Text.enabled) return;
+
+            if (Camera == null)
+                Camera = !HudManager.InstanceExists ? Camera.main : HudManager.Instance.PlayerCam.GetComponent<Camera>();
+            if (Camera != null)
+            {
+                transform.position = AspectPosition.ComputeWorldPosition(Camera, AspectPosition.EdgeAlignments.Top, TextOffset);
+            }
+        }
     }
 }
