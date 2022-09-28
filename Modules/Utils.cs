62a3eb7351eafe2c15d710e6e2de84c918936781
tanakanira0118 <github@tanakanira.com>
2022-09-28 17:26:35 +0900

    ColorStringとToByteをUtilsに移動

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -1029,5 +1029,15 @@ namespace TownOfHost
                 obj.GetComponent<SpriteRenderer>().color = new(color.r, color.g, color.b, Mathf.Clamp01((-2f * Mathf.Abs(t - 0.5f) + 1) * color.a)); //アルファ値を0→目標→0に変化させる
             })));
         }
+        public static string ColorString(Color c, string s)
+        {
+            return $"<color=#{ToByte(c.r):X2}{ToByte(c.g):X2}{ToByte(c.b):X2}{ToByte(c.a):X2}>{s}</color>";
+        }
+
+        private static byte ToByte(float f)
+        {
+            f = Mathf.Clamp01(f);
+            return (byte)(f * 255);
+        }
     }
 }
