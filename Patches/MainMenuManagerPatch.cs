0d34494398d1d564dad6ff7ab31b0575a026fda6
tanakanira0118 <github@tanakanira.com>
2022-10-14 18:56:08 +0900

    終了ボタンを取得

--- Patches/MainMenuManagerPatch.cs
+++ Patches/MainMenuManagerPatch.cs
@@ -7,9 +7,12 @@ namespace TownOfHost
     [HarmonyPatch]
     public class MainMenuManagerPatch
     {
+        public static GameObject template;
         [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start)), HarmonyPrefix]
         public static void Start_Prefix(MainMenuManager __instance)
         {
+            if (template == null) template = GameObject.Find("/MainUI/ExitGameButton");
+            if (template == null) return;
 
         }
     }
