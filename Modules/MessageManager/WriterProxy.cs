88b391191f5735c510079b4fbd5ae4a5a64e7413
EmptyBottle <tts13511@hotmail.com>
2022-10-19 22:24:14 +0900

    writerをプロパティに変更し、外部からの取得を許可

--- Modules/MessageManager/WriterProxy.cs
+++ Modules/MessageManager/WriterProxy.cs
@@ -7,28 +7,28 @@ namespace TownOfHost
 {
     public class WriterProxy : ICustomWriter
     {
-        private MessageWriter writer;
+        public MessageWriter Writer { get; private set; }
         private WriterProxy() { }
 
         public WriterProxy(MessageWriter writer)
         {
-            this.writer = writer;
+            this.Writer = writer;
         }
 
-        public void Write(float val) => writer.Write(val);
-        public void Write(string val) => writer.Write(val);
-        public void Write(ulong val) => writer.Write(val);
-        public void Write(int val) => writer.Write(val);
-        public void Write(uint val) => writer.Write(val);
-        public void Write(ushort val) => writer.Write(val);
-        public void Write(byte val) => writer.Write(val);
-        public void Write(sbyte val) => writer.Write(val);
-        public void Write(bool val) => writer.Write(val);
-        public void Write(Il2CppStructArray<byte> bytes) => writer.Write(bytes);
-        public void Write(Il2CppStructArray<byte> bytes, int offset, int length) => writer.Write(bytes, offset, length);
-        public void WriteBytesAndSize(Il2CppStructArray<byte> bytes) => writer.WriteBytesAndSize(bytes);
-        public void WritePacked(int val) => writer.WritePacked(val);
-        public void WritePacked(uint val) => writer.WritePacked(val);
-        public void WriteNetObject(InnerNetObject obj) => writer.WriteNetObject(obj);
+        public void Write(float val) => Writer.Write(val);
+        public void Write(string val) => Writer.Write(val);
+        public void Write(ulong val) => Writer.Write(val);
+        public void Write(int val) => Writer.Write(val);
+        public void Write(uint val) => Writer.Write(val);
+        public void Write(ushort val) => Writer.Write(val);
+        public void Write(byte val) => Writer.Write(val);
+        public void Write(sbyte val) => Writer.Write(val);
+        public void Write(bool val) => Writer.Write(val);
+        public void Write(Il2CppStructArray<byte> bytes) => Writer.Write(bytes);
+        public void Write(Il2CppStructArray<byte> bytes, int offset, int length) => Writer.Write(bytes, offset, length);
+        public void WriteBytesAndSize(Il2CppStructArray<byte> bytes) => Writer.WriteBytesAndSize(bytes);
+        public void WritePacked(int val) => Writer.WritePacked(val);
+        public void WritePacked(uint val) => Writer.WritePacked(val);
+        public void WriteNetObject(InnerNetObject obj) => Writer.WriteNetObject(obj);
     }
 }
