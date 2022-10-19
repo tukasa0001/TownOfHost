0f688ba73f69f319cc325556616cb0ea1322efcf
そうくん <daoyecangma@gmail.com>
2022-10-19 13:30:52 +0900

    ResetPlayerCamも既存のメソッドを使うように

--- Modules/ExtendedPlayerControl.cs
+++ Modules/ExtendedPlayerControl.cs
@@ -453,45 +453,21 @@ namespace TownOfHost
         public static void ResetPlayerCam(this PlayerControl pc, float delay = 0f)
         {
             if (pc == null || !AmongUsClient.Instance.AmHost || pc.AmOwner) return;
-            int clientId = pc.GetClientId();
 
-            byte reactorId = 3;
-            if (PlayerControl.GameOptions.MapId == 2) reactorId = 21;
-
-            new LateTask(() =>
-            {
-                MessageWriter SabotageWriter = AmongUsClient.Instance.StartRpcImmediately(ShipStatus.Instance.NetId, (byte)RpcCalls.RepairSystem, SendOption.Reliable, clientId);
-                SabotageWriter.Write(reactorId);
-                MessageExtensions.WriteNetObject(SabotageWriter, pc);
-                SabotageWriter.Write((byte)128);
-                AmongUsClient.Instance.FinishRpcImmediately(SabotageWriter);
-            }, 0f + delay, "Reactor Desync");
+            var systemtypes = SystemTypes.Reactor;
+            if (PlayerControl.GameOptions.MapId == 2) systemtypes = SystemTypes.Laboratory;
 
             new LateTask(() =>
             {
-                MessageWriter MurderWriter = AmongUsClient.Instance.StartRpcImmediately(pc.NetId, (byte)RpcCalls.MurderPlayer, SendOption.Reliable, clientId);
-                MessageExtensions.WriteNetObject(MurderWriter, pc);
-                AmongUsClient.Instance.FinishRpcImmediately(MurderWriter);
-            }, 0.2f + delay, "Murder To Reset Cam");
 
-            new LateTask(() =>
-            {
-                MessageWriter SabotageFixWriter = AmongUsClient.Instance.StartRpcImmediately(ShipStatus.Instance.NetId, (byte)RpcCalls.RepairSystem, SendOption.Reliable, clientId);
-                SabotageFixWriter.Write(reactorId);
-                MessageExtensions.WriteNetObject(SabotageFixWriter, pc);
-                SabotageFixWriter.Write((byte)16);
-                AmongUsClient.Instance.FinishRpcImmediately(SabotageFixWriter);
-            }, 0.4f + delay, "Fix Desync Reactor");
+                pc.RpcDesyncRepairSystem(systemtypes, 128);
+                pc.RpcSpecificMurderPlayer();
+                pc.RpcDesyncRepairSystem(systemtypes, 16);
 
-            if (PlayerControl.GameOptions.MapId == 4) //Airship用
-                new LateTask(() =>
-                {
-                    MessageWriter SabotageFixWriter = AmongUsClient.Instance.StartRpcImmediately(ShipStatus.Instance.NetId, (byte)RpcCalls.RepairSystem, SendOption.Reliable, clientId);
-                    SabotageFixWriter.Write(reactorId);
-                    MessageExtensions.WriteNetObject(SabotageFixWriter, pc);
-                    SabotageFixWriter.Write((byte)17);
-                    AmongUsClient.Instance.FinishRpcImmediately(SabotageFixWriter);
-                }, 0.4f + delay, "Fix Desync Reactor 2");
+                if (PlayerControl.GameOptions.MapId == 4) //Airship用
+                    pc.RpcDesyncRepairSystem(systemtypes, 17);
+
+            }, 0f + delay, "ResetPlayerCam");
         }
         public static void ReactorFlash(this PlayerControl pc, float delay = 0f)
         {
