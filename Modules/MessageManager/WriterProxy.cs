887495e4bb9ab8d7888402daea9e2c815f39a0a7
EmptyBottle <tts13511@hotmail.com>
2022-10-19 22:23:12 +0900

    ICustomWriterと代理書き込み関数を実装

--- Modules/MessageManager/WriterProxy.cs
+++ Modules/MessageManager/WriterProxy.cs
@@ -5,7 +5,7 @@ using UnhollowerBaseLib;
 
 namespace TownOfHost
 {
-    public class WriterProxy
+    public class WriterProxy : ICustomWriter
     {
         private MessageWriter writer;
         private WriterProxy() { }
@@ -14,5 +14,21 @@ namespace TownOfHost
         {
             this.writer = writer;
         }
+
+        public void Write(float val) => writer.Write(val);
+        public void Write(string val) => writer.Write(val);
+        public void Write(ulong val) => writer.Write(val);
+        public void Write(int val) => writer.Write(val);
+        public void Write(uint val) => writer.Write(val);
+        public void Write(ushort val) => writer.Write(val);
+        public void Write(byte val) => writer.Write(val);
+        public void Write(sbyte val) => writer.Write(val);
+        public void Write(bool val) => writer.Write(val);
+        public void Write(Il2CppStructArray<byte> bytes) => writer.Write(bytes);
+        public void Write(Il2CppStructArray<byte> bytes, int offset, int length) => writer.Write(bytes, offset, length);
+        public void WriteBytesAndSize(Il2CppStructArray<byte> bytes) => writer.WriteBytesAndSize(bytes);
+        public void WritePacked(int val) => writer.WritePacked(val);
+        public void WritePacked(uint val) => writer.WritePacked(val);
+        public void WriteNetObject(InnerNetObject obj) => writer.WriteNetObject(obj);
     }
 }
