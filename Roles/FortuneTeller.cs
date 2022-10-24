27e5ba973b335d42ed051f37f557fd174561d174
schwKatz <katz.schw@gmail.com>
2022-10-24 19:07:10 +0900

    占い結果の格納部分作成

--- Roles/FortuneTeller.cs
+++ Roles/FortuneTeller.cs
@@ -9,6 +9,7 @@ namespace TownOfHost
         public static List<byte> playerIdList = new();
 
         public static Dictionary<byte, PlayerControl> Target = new();
+        public static Dictionary<byte, Dictionary<byte, PlayerControl>> TargetResult = new();
 
         public static void SetupCustomOption()
         {
@@ -18,6 +19,7 @@ namespace TownOfHost
         {
             playerIdList = new();
             Target = new();
+            TargetResult = new();
         }
         public static void Add(byte playerId)
         {
@@ -31,14 +33,14 @@ namespace TownOfHost
                 Logger.Info($"VoteForecastTarget NotForecast NoDeadBody player: {player.name}, targetId: {targetId}", "FortuneTeller");
                 return;
             }
-
+ 
             player.SetForecastTarget(targetId);
         }
         public static void SetForecastTarget(this PlayerControl player, byte targetId)
         {
             var target = Utils.GetPlayerById(targetId);
             if (target == null || target.Data.IsDead || target.Data.Disconnected) return;
-            //todo:既に占い結果があるときはターゲットにならない
+            if (player.HasForecastResult(target.PlayerId)) return;  //既に占い結果があるときはターゲットにならない
 
             Target[player.PlayerId] = target;
             Logger.Info($"SetForecastTarget player: {player.name}, target: {target.name}", "FortuneTeller");
@@ -47,6 +49,35 @@ namespace TownOfHost
         {
             if (!Target.TryGetValue(player.PlayerId, out var target)) return false;
             return target != null;
+        }
+        public static void SetForecastResult(this PlayerControl player)
+        {
+            if (!Target.TryGetValue(player.PlayerId, out var target))
+            {
+                Logger.Info($"SetForecastResult NotSet NotHasForecastTarget player: {player.name}", "FortuneTeller");
+                return;
+            }
+            Target.Remove(player.PlayerId);
+
+            if (target == null || target.Data.IsDead || target.Data.Disconnected)
+            {
+                Logger.Info($"SetForecastResult NotSet TargetNotValid player: {player.name}, target: {target?.name} dead: {target?.Data.IsDead}, disconnected: {target?.Data.Disconnected}", "FortuneTeller");
+                return;
+            }
+
+            if (!TargetResult.TryGetValue(player.PlayerId, out var resultTarget))
+            {
+                resultTarget = new();
+                TargetResult[player.PlayerId] = resultTarget;
+            }
+
+            resultTarget[target.PlayerId] = target;
+            Logger.Info($"SetForecastResult SetTarget player: {player.name}, target: {target.name}", "FortuneTeller");
+        }
+         public static bool HasForecastResult(this PlayerControl player, byte targetId)
+        {
+            if (!TargetResult.TryGetValue(player.PlayerId, out var resultTarget)) return false;
+            return resultTarget.ContainsKey(targetId);
         }
     }
 }
