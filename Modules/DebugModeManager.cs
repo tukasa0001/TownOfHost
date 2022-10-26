97eb09484fad704b5283d798bbf53a82568111ae
EmptyBottle <tts13511@hotmail.com>
2022-10-26 22:01:46 +0900

    フィールド・プロパティに関するコメントを追加

--- Modules/DebugModeManager.cs
+++ Modules/DebugModeManager.cs
@@ -8,12 +8,15 @@ namespace TownOfHost
 {
     public static class DebugModeManager
     {
+        // これが有効の時、通常のゲームに支障のないデバッグ機能(詳細ログ・ゲーム外でのデバッグ表示など)が有効化される。
+        // また、ゲーム内オプションでデバッグモードを有効化することができる。
         public static readonly bool AmDebugger =
 #if DEBUG
     true;
 #else
     false;
 #endif
+        // これが有効の時、通常のゲームを破壊する可能性のある強力なデバッグ機能(テレポートなど)が有効化される。
         public static bool IsDebugMode => EnableDebugMode != null && EnableDebugMode.GetBool();
 
         public static CustomOption EnableDebugMode;
