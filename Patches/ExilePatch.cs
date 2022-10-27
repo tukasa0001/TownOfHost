4539692ee626aaebfccf33a9cc586b35d056babb
Masami4711 <103577031+Masami4711@users.noreply.github.com>
2022-10-27 20:19:23 +0900

    会議後自殺でRealKiller追加

--- Patches/ExilePatch.cs
+++ Patches/ExilePatch.cs
@@ -107,6 +107,8 @@ namespace TownOfHost
                 PlayerState.SetDeathReason(x.Key, x.Value);
                 PlayerState.SetDead(x.Key);
                 player?.RpcExileV2();
+                if (x.Value == PlayerState.DeathReason.Suicide)
+                    player?.SetRealKiller(player, true);
                 if (player.Is(CustomRoles.TimeThief) && x.Value == PlayerState.DeathReason.FollowingSuicide)
                     player?.ResetVotingTime();
                 if (Executioner.Target.ContainsValue(x.Key))
