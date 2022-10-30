a495daba197404ff2fbf2a1c8238016d8f4f6891
EmptyBottle <tts13511@hotmail.com>
2022-10-31 02:43:16 +0900

    ハッシュ値を比較する関数とhash値計算用の関数を追加

--- Modules/HashAuth.cs
+++ Modules/HashAuth.cs
@@ -23,5 +23,29 @@ namespace TownOfHost
             this.salt = salt;
             algorithm = SHA256.Create();
         }
+
+        public bool CheckString(string value)
+        {
+            var hash = CalculateHash(value);
+            return hash == value;
+        }
+        private string CalculateHash(string source)
+        {
+            // 1.saltの適用
+            if (salt != null) source += salt;
+
+            // 2.sourceをbyte配列に変換
+            var sourceBytes = Encoding.UTF8.GetBytes(source);
+
+            // 3.sourceBytesをハッシュ化
+            var hashBytes = algorithm.ComputeHash(sourceBytes);
+
+            // 4.hashBytesを文字列化
+            var sb = new StringBuilder();
+            foreach (var b in hashBytes)
+                sb.Append(b.ToString("x2")); //1byteずつ2桁の16進法表記に変換する
+
+            return sb.ToString();
+        }
     }
 }
