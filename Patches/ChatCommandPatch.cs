a08fb8e3528f315382797e1da95776df4ab3e610
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2022-11-03 21:28:53 +0900

    チャットコマンド

--- Patches/ChatCommandPatch.cs
+++ Patches/ChatCommandPatch.cs
@@ -300,6 +300,7 @@ namespace TownOfHost
                 { CustomRoles.SchrodingerCat, "sc" },
                 { CustomRoles.Terrorist, "te" },
                 { CustomRoles.Jackal, "jac" },
+                { CustomRoles.Thief, "th" },
                 //Sub役職
                 { (CustomRoles)(-6), $"== {GetString("SubRole")} ==" }, //区切り用
                 {CustomRoles.Lovers, "lo" },
