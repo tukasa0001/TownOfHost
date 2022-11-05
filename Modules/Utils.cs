5dee42cc50c59956d9229d26c6454b8970cff5cb
EmptyBottle <tts13511@hotmail.com>
2022-11-05 20:14:27 +0900

    乱数のヒストグラムを生成する関数を追加

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -160,7 +160,7 @@ namespace TownOfHost
             var roleName = GetRoleName(role);
             if (role.IsImpostor() && role != CustomRoles.LastImpostor && Utils.IsLastImpostor(playerId))
             {
-                roleName = GetRoleString("Last")+ " " + roleName;
+                roleName = GetRoleString("Last") + " " + roleName;
             }
 
             return roleName;
@@ -1088,6 +1088,41 @@ namespace TownOfHost
             float G = (color.g + Weight) / (Darkness + 1);
             float B = (color.b + Weight) / (Darkness + 1);
             return new Color(R, G, B, color.a);
+        }
+
+        /// <summary>
+        /// 乱数の簡易的なヒストグラムを取得する関数
+        /// <params name="nums">生成した乱数を格納したint配列</params>
+        /// <params name="scale">ヒストグラムの倍率 大量の乱数を扱う場合、この値を下げることをお勧めします。</params>
+        /// </summary>
+        public static string WriteRandomHistgram(int[] nums, float scale = 1.0f)
+        {
+            int[] countData = new int[nums.Max() + 1];
+            foreach (var num in nums)
+            {
+                if (0 <= num) countData[num]++;
+            }
+            StringBuilder sb = new();
+            for (int i = 0; i < countData.Length; i++)
+            {
+                // 倍率適用
+                countData[i] = (int)(countData[i] * scale);
+
+                // 行タイトル
+                sb.AppendFormat("{0:D2}", i).Append(" : ");
+
+                // ヒストグラム部分
+                for (int j = 0; j < countData[i]; j++)
+                    sb.Append('|');
+
+                // 改行
+                sb.Append('\n');
+            }
+
+            // その他の情報
+            sb.Append("最大数 - 最小数: ").Append(countData.Max() - countData.Min());
+
+            return sb.ToString();
         }
     }
 }
