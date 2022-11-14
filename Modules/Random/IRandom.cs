7d28862f05e6d2565aef1661a9aa721286aff1fb
EmptyBottle <51523918+tukasa0001@users.noreply.github.com>
2022-11-14 13:21:52 +0900

    IRandomを実装するクラスのリストを追加

--- Modules/Random/IRandom.cs
+++ Modules/Random/IRandom.cs
@@ -10,6 +10,15 @@ namespace TownOfHost
         public int Next(int minValue, int maxValue);
 
         // == static ==
+        // IRandomを実装するクラスのリスト
+        public static Dictionary<int, Type> randomTypes = new()
+        {
+            {0, typeof(NetRandomWrapper)}, //Default
+            {1, typeof(NetRandomWrapper)},
+            {2, typeof(HashRandomWrapper)},
+            {3, typeof(Xorshift)}
+        };
+        
         public static IRandom Instance { get; private set; }
         public static void SetInstance(IRandom instance)
         {
