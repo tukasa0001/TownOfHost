057131d639b642f71c25296c692146a1e82cabaa
Yumenopai <107914133+Yumenopai@users.noreply.github.com>
2022-11-22 19:55:07 +0900

    AntiAdminer.Add(target)追加

--- Modules/RPC.cs
+++ Modules/RPC.cs
@@ -331,6 +331,9 @@ namespace TownOfHost
                 case CustomRoles.SabotageMaster:
                     SabotageMaster.Add(targetId);
                     break;
+                case CustomRoles.AntiAdminer:
+                    AntiAdminer.Add(targetId);
+                    break;
             }
             HudManager.Instance.SetHudActive(true);
         }
