c8952e43f552a9ff0cc26706161c22ee74f96d1a
EmptyBottle <tts13511@hotmail.com>
2022-11-24 03:16:20 +0900

    主要なフィールド・プロパティを定義

--- Modules/OptionItem/OptionItem.cs
+++ Modules/OptionItem/OptionItem.cs
@@ -9,6 +9,35 @@ namespace TownOfHost
 {
     public abstract class OptionItem
     {
+        #region static
+        public static IReadOnlyList<OptionItem> AllOptions => _allOptions;
+        private static List<OptionItem> _allOptions = new();
+        public static int CurrentPreset { get; set; }
+        #endregion
+
+        // 必須情報 (コンストラクタで必ず設定させる必要がある値)
+        public int Id { get; }
+        public string Name { get; }
+        public int DefaultValue { get; }
+        public TabGroup Tab { get; }
+        public bool IsSingleValue { get; }
+
+        // 任意情報 (空・nullを許容する または、ほとんど初期値で問題ない値)
+        public Color NameColor { get; protected set; }
+        public OptionFormat ValueFormat { get; protected set; }
+        public CustomGameMode GameMode { get; protected set; }
+        public bool IsHeader { get; protected set; }
+        public bool IsHidden { get; protected set; }
+        public Dictionary<string, string> ReplacementDictionary
+        {
+            get => _replacementDictionary;
+            set
+            {
+                if (value == null) _replacementDictionary?.Clear();
+                else _replacementDictionary = value;
+            }
+        }
+        private Dictionary<string, string> _replacementDictionary;
     }
 
     public enum TabGroup
