274a16523ae87b2c305f49e459d58e1f33cc7c5c
EmptyBottle <tts13511@hotmail.com>
2022-11-24 03:43:24 +0900

    古いコメントを削除

--- Modules/OptionItem/ValueRule.cs
+++ Modules/OptionItem/ValueRule.cs
@@ -18,11 +18,6 @@ namespace TownOfHost
         : this(tuple.Item1, tuple.Item2, tuple.Item3)
         { }
 
-        /// <summary>
-        /// 引数に指定された値が範囲外の場合、折り返した値を返します。
-        /// </summary>
-        // 例: (2, 12, 2)の時、0から順番に値を入力した場合:
-        //     0, 1, 2, 3, 4, 0, 1, 2...
         public abstract int RepeatIndex(int value);
         public abstract T GetValueByIndex(int index);
         public abstract int GetNearestIndex(T num);
