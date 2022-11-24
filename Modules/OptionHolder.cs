56bf26eb24cf6c8f1e6f9e46c1681e15c473042b
tanakanira0118 <github@tanakanira.com>
2022-11-24 13:20:22 +0900

    RoleAssigningAlgorithmの引数を修正

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -595,7 +595,7 @@ namespace TownOfHost
                 .SetGameMode(CustomGameMode.All);
             ChangeNameToRoleInfo = OptionItem.Create(1_000_004, TabGroup.MainSettings, Color.white, "ChangeNameToRoleInfo", true)
                 .SetGameMode(CustomGameMode.All);
-            RoleAssigningAlgorithm = OptionItem.Create(1_000_005, TabGroup.MainSettings, Color.white, "RoleAssigningAlgorithm", RoleAssigningAlgorithms, RoleAssigningAlgorithms[0])
+            RoleAssigningAlgorithm = OptionItem.Create(1_000_005, TabGroup.MainSettings, Color.white, "RoleAssigningAlgorithm", RoleAssigningAlgorithms, 0)
                 .SetGameMode(CustomGameMode.All)
                 .RegisterUpdateValueEvent(
                     (object obj, OptionItem.UpdateValueEventArgs args) => IRandom.SetInstanceById(args.CurrentValue)
