e7d03dc28575142055f9040eedea176275007fc2
tanakanira0118 <github@tanakanira.com>
2022-11-24 13:04:33 +0900

    不要なusingを削除

--- Modules/OptionItem/BooleanOptionItem.cs
+++ Modules/OptionItem/BooleanOptionItem.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using BepInEx.Configuration;
-using UnityEngine;
-using TownOfHost;
-
 namespace TownOfHost
 {
     public class BooleanOptionItem : OptionItem
--- Modules/OptionItem/OptionItem.cs
+++ Modules/OptionItem/OptionItem.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using BepInEx.Configuration;
 using UnityEngine;
-using TownOfHost;
 
 namespace TownOfHost
 {
