b00f8aba70137a7d2acbf7d5a864f2e270298838
EmptyBottle <tts13511@hotmail.com>
2022-12-10 18:34:40 +0900

    OptionBackupDataクラスを作成

---
+++ Modules/OptionBackup/OptionBackupData.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AmongUs.GameOptions;
+
+
+namespace TownOfHost
+{
+    public class OptionBackupData
+    {
+        public OptionBackupData(IGameOptions option)
+        {
+
+        }
+    }
+}
