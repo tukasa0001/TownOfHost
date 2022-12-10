7641d7de6acf7b66448329db819a1f06b65fd510
EmptyBottle <tts13511@hotmail.com>
2022-12-10 18:59:21 +0900

    各型のBackupValueクラスを追加 型制限を修正

--- Modules/OptionBackup/OptionBackupValue.cs
+++ Modules/OptionBackup/OptionBackupValue.cs
@@ -13,7 +13,6 @@ namespace TownOfHost
 
     public abstract class OptionBackupValueBase<NameT, ValueT> : OptionBackupValue
     where NameT : Enum
-    where ValueT : struct
     {
         public readonly NameT OptionName;
         public readonly ValueT Value;
@@ -21,6 +20,55 @@ namespace TownOfHost
         {
             OptionName = name;
             Value = value;
+        }
+    }
+
+    public class ByteOptionBackupValue : OptionBackupValueBase<ByteOptionNames, byte>
+    {
+        public ByteOptionBackupValue(ByteOptionNames name, byte value) : base(name, value) { }
+        public override void Restore(IGameOptions option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BoolOptionBackupValue : OptionBackupValueBase<BoolOptionNames, bool>
+    {
+        public BoolOptionBackupValue(BoolOptionNames name, bool value) : base(name, value) { }
+        public override void Restore(IGameOptions option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class FloatOptionBackupValue : OptionBackupValueBase<FloatOptionNames, float>
+    {
+        public FloatOptionBackupValue(FloatOptionNames name, float value) : base(name, value) { }
+        public override void Restore(IGameOptions option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class IntOptionBackupValue : OptionBackupValueBase<Int32OptionNames, int>
+    {
+        public IntOptionBackupValue(Int32OptionNames name, int value) : base(name, value) { }
+        public override void Restore(IGameOptions option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class IntArrayOptionBackupValue : OptionBackupValueBase<Int32ArrayOptionNames, int[]>
+    {
+        public IntArrayOptionBackupValue(Int32ArrayOptionNames name, int[] value) : base(name, value) { }
+        public override void Restore(IGameOptions option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class FloatArrayOptionBackupValue : OptionBackupValueBase<FloatArrayOptionNames, float[]>
+    {
+        public FloatArrayOptionBackupValue(FloatArrayOptionNames name, float[] value) : base(name, value) { }
+        public override void Restore(IGameOptions option)
+        {
+            throw new NotImplementedException();
         }
     }
 }
