46f98379f7d8df2ed1affa3c02ab9c3b5f707d8c
EmptyBottle <tts13511@hotmail.com>
2022-12-10 19:10:59 +0900

    OptionBackupDataのコンストラクタを作成

--- Modules/OptionBackup/OptionBackupData.cs
+++ Modules/OptionBackup/OptionBackupData.cs
@@ -8,9 +8,31 @@ namespace TownOfHost
 {
     public class OptionBackupData
     {
+        public List<OptionBackupValue> AllValues;
         public OptionBackupData(IGameOptions option)
         {
+            AllValues = new(32);
 
+            foreach (ByteOptionNames name in Enum.GetValues(typeof(ByteOptionNames)))
+            {
+                if (option.TryGetByte(name, out var value))
+                    AllValues.Add(new ByteOptionBackupValue(name, value));
+            }
+            foreach (BoolOptionNames name in Enum.GetValues(typeof(BoolOptionNames)))
+            {
+                if (option.TryGetBool(name, out var value))
+                    AllValues.Add(new BoolOptionBackupValue(name, value));
+            }
+            foreach (FloatOptionNames name in Enum.GetValues(typeof(FloatOptionNames)))
+            {
+                if (option.TryGetFloat(name, out var value))
+                    AllValues.Add(new FloatOptionBackupValue(name, value));
+            }
+            foreach (Int32OptionNames name in Enum.GetValues(typeof(Int32OptionNames)))
+            {
+                if (option.TryGetInt(name, out var value))
+                    AllValues.Add(new IntOptionBackupValue(name, value));
+            }
         }
     }
 }
