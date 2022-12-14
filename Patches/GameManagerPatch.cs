af02c6d7f0ce8e29be1dd79c6c973eb962572f72
EmptyBottle <tts13511@hotmail.com>
2022-12-14 20:08:10 +0900

    GameManager.SerializeへのPatchを追加 処理内容はバニラと同じ

--- Patches/GameManagerPatch.cs
+++ Patches/GameManagerPatch.cs
@@ -3,6 +3,29 @@ using Hazel;
 
 namespace TownOfHost
 {
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.Serialize))]
+    class GameManagerSerializeFix
+    {
+        public static bool Prefix(GameManager __instance, [HarmonyArgument(0)] MessageWriter writer, [HarmonyArgument(1)] bool initialState, ref bool __result)
+        {
+            bool flag = false;
+            for (int index = 0; index < __instance.LogicComponents.Count; ++index)
+            {
+                GameLogicComponent logicComponent = __instance.LogicComponents[index];
+                if (initialState || logicComponent.IsDirty)
+                {
+                    flag = true;
+                    writer.StartMessage((byte)index);
+                    logicComponent.Serialize(writer, initialState);
+                    writer.EndMessage();
+                    logicComponent.ClearDirtyFlag();
+                }
+            }
+            __instance.ClearDirtyBits();
+            __result = flag;
+            return false;
+        }
+    }
     [HarmonyPatch(typeof(LogicOptions), nameof(LogicOptions.Serialize))]
     class LogicOptionsSerializePatch
     {
