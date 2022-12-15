e0d9e08caf68381d01aef2c663070b50bba0cfa8
EmptyBottle <tts13511@hotmail.com>
2022-12-15 20:58:02 +0900

    引数をdynamicに変更

--- Modules/Extensions/IGameManagerEx.cs
+++ Modules/Extensions/IGameManagerEx.cs
@@ -4,10 +4,10 @@ namespace TownOfHost.Modules.Extensions
 {
     public static class IGameManagerEx
     {
-        public static void Set(this BoolOptionNames name, bool value, IGameOptions opt) => opt.SetBool(name, value);
-        public static void Set(this Int32OptionNames name, int value, IGameOptions opt) => opt.SetInt(name, value);
-        public static void Set(this FloatOptionNames name, float value, IGameOptions opt) => opt.SetFloat(name, value);
-        public static void Set(this ByteOptionNames name, byte value, IGameOptions opt) => opt.SetByte(name, value);
-        public static void Set(this UInt32OptionNames name, uint value, IGameOptions opt) => opt.SetUInt(name, value);
+        public static void Set(this BoolOptionNames name, bool value, dynamic opt) => opt.SetBool(name, value);
+        public static void Set(this Int32OptionNames name, int value, dynamic opt) => opt.SetInt(name, value);
+        public static void Set(this FloatOptionNames name, float value, dynamic opt) => opt.SetFloat(name, value);
+        public static void Set(this ByteOptionNames name, byte value, dynamic opt) => opt.SetByte(name, value);
+        public static void Set(this UInt32OptionNames name, uint value, dynamic opt) => opt.SetUInt(name, value);
     }
 }
