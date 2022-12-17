67e078cdc9bc6ebcb66f2ab0be897027f27c5eba
EmptyBottle <tts13511@hotmail.com>
2022-12-17 14:25:42 +0900

    関数分割

--- Modules/GameOptionsSender/GameOptionsSender.cs
+++ Modules/GameOptionsSender/GameOptionsSender.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using Il2CppSystem.Linq;
 using InnerNet;
 using Hazel;
 using AmongUs.GameOptions;
@@ -26,11 +27,17 @@ namespace TownOfHost.Modules
         public virtual void SendGameOptions()
         {
             var opt = BuildGameOptions();
+
+            SendOptionsArray(AmongUsClient.Instance.gameOptionsFactory.ToBytes(opt));
+        }
+        protected virtual void SendOptionsArray(byte[] optionArray, int targetClientId = -1)
+        {
             var writer = MessageWriter.Get(SendOption.Reliable);
 
-            writer.StartMessage(Tags.GameData);
+            writer.StartMessage(targetClientId == -1 ? Tags.GameData : Tags.GameDataTo);
             {
                 writer.Write(AmongUsClient.Instance.GameId);
+                if (targetClientId != -1) writer.WritePacked(targetClientId);
                 writer.StartMessage(1);
                 {
                     writer.WritePacked(GameManager.Instance.NetId);
@@ -40,7 +47,7 @@ namespace TownOfHost.Modules
                         if (GameManager.Instance.LogicComponents[i] is LogicOptions lo)
                         {
                             writer.StartMessage((byte)i);
-                            writer.WriteBytesAndSize(lo.gameOptionsFactory.ToBytes(opt));
+                            writer.WriteBytesAndSize(optionArray);
                             writer.EndMessage();
                         }
                     }
@@ -48,6 +55,9 @@ namespace TownOfHost.Modules
                 }
                 writer.EndMessage();
             }
+
+            AmongUsClient.Instance.SendOrDisconnect(writer);
+            writer.Recycle();
         }
         public abstract IGameOptions BuildGameOptions();
     }
