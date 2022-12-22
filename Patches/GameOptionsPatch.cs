efbf6bdf7667ff8677d10870676a3d200e0233e1
EmptyBottle <tts13511@hotmail.com>
2022-12-22 18:26:58 +0900

    アンロード処理を追加

--- Patches/GameOptionsPatch.cs
+++ Patches/GameOptionsPatch.cs
@@ -48,4 +48,19 @@ namespace TownOfHost
             }
         }
     }
+
+    [HarmonyPatch(typeof(GameOptionsManager), nameof(GameOptionsManager.SwitchGameMode))]
+    class SwitchGameModePatch
+    {
+        public static void Postfix(GameModes gameMode)
+        {
+            if (gameMode == GameModes.HideNSeek)
+            {
+                ErrorText.Instance.HnSFlag = true;
+                ErrorText.Instance.AddError(ErrorCode.HnsUnload);
+                Harmony.UnpatchAll();
+                Main.Instance.Unload();
+            }
+        }
+    }
 }
