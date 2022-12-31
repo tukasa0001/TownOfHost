7cee5b20c82f1f462ffa69a5a4273536c79f2192
じゅきまぐろ <coountry05300727@gmail.com>
2023-01-01 03:35:43 +0900

    動作部を作成

--- Roles/TimeManager.cs
+++ Roles/TimeManager.cs
@@ -19,5 +19,52 @@ namespace TownOfHost
             MeetingTimeLimit = FloatOptionItem.Create(Id + 11, "TimeManagerLimitMeetingTime", new(300f, 150f, 500f), 1f, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.TimeManager])
                 .SetValueFormat(OptionFormat.Seconds);
         }
+        public static void Init()
+        {
+            TimeManagerTaskCount = new();
+            playerIdList = new();
+        }
+        public static void Add(byte playerId)
+        {
+            playerIdList.Add(playerId);
+            TimeManagerTaskCount[playerId] = 0;
+            Utils.GetPlayerById(playerId)?.RpcSetTimeManagerTaskCount();
+        }
+        public static bool IsEnable() => playerIdList.Count > 0;
+        public static void ReceiveRPC(MessageReader msg)
+        {
+            byte TimeManagerId = msg.ReadByte();
+            int TimeManagerTaskCount = msg.ReadInt32();
+            if (TimeManager.TimeManagerTaskCount.ContainsKey(TimeManagerId))
+                TimeManager.TimeManagerTaskCount[TimeManagerId] = TimeManagerTaskCount;
+            else
+                TimeManager.TimeManagerTaskCount.Add(TimeManagerId, 0);
+            Logger.Info($"Player{TimeManagerId}:ReceiveRPC", "TimeManager");
+        }
+        public static void RpcSetTimeManagerTaskCount(this PlayerControl player)
+        {
+            if (!AmongUsClient.Instance.AmHost) return;
+            MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.SetTimeManagerTaskCount, Hazel.SendOption.Reliable, -1);
+            writer.Write(player.PlayerId);
+            writer.Write(TimeManagerTaskCount[player.PlayerId]);
+            AmongUsClient.Instance.FinishRpcImmediately(writer);
+        }
+        public static void TimeManagerResetVotingTime(this PlayerControl timemanager)
+        {
+            for (var i = 0; i < TimeManagerTaskCount[timemanager.PlayerId]; i++)
+                Main.VotingTime -= IncreaseMeetingTime.GetInt();
+            TimeManagerTaskCount[timemanager.PlayerId] = 0; //会議時間の初期化
+        }
+        public static void OnCheckCompleteTask(PlayerControl player)
+        {
+            TimeManagerTaskCount[player.PlayerId]++;
+            player.RpcSetTimeManagerTaskCount();
+            Main.DiscussionTime += IncreaseMeetingTime.GetInt();//会議時間に増える分の会議時間を加算
+            if (Main.DiscussionTime > 0)
+            {
+                Main.VotingTime += Main.DiscussionTime;
+                Main.DiscussionTime = 0;//投票時間+会議時間を投票時間とし、会議時間を0にする
+            }
+        }
     }
 }
