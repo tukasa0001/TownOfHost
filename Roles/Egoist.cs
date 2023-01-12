12b23bfa7f11602aa7b74d585bc3f4c341d43cc3
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2023-01-12 23:48:41 +0900

    オプション形式の変更

--- Roles/Egoist.cs
+++ Roles/Egoist.cs
@@ -8,19 +8,23 @@ namespace TownOfHost
         private static readonly int Id = 50600;
         public static List<byte> playerIdList = new();
 
-        private static OptionItem KillCooldown;
-        public static OptionItem CanCreateMadmate;
+        static OptionItem OptionKillCooldown;
+        static OptionItem OptionCanCreateMadmate;
 
+        private static float KillCooldown;
+        public static bool CanCreateMadmate;
         public static void SetupCustomOption()
         {
             Options.SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Egoist);
-            KillCooldown = FloatOptionItem.Create(Id + 10, "KillCooldown", new(2.5f, 180f, 2.5f), 20f, TabGroup.NeutralRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Egoist])
+            OptionKillCooldown = FloatOptionItem.Create(Id + 10, "KillCooldown", new(2.5f, 180f, 2.5f), 20f, TabGroup.NeutralRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Egoist])
                 .SetValueFormat(OptionFormat.Seconds);
-            CanCreateMadmate = BooleanOptionItem.Create(Id + 11, "CanCreateMadmate", false, TabGroup.NeutralRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Egoist]);
+            OptionCanCreateMadmate = BooleanOptionItem.Create(Id + 11, "CanCreateMadmate", false, TabGroup.NeutralRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Egoist]);
         }
         public static void Init()
         {
             playerIdList = new();
+            KillCooldown = OptionKillCooldown.GetFloat();
+            CanCreateMadmate = OptionCanCreateMadmate.GetBool();
         }
         public static void Add(byte ego)
         {
@@ -28,7 +32,7 @@ namespace TownOfHost
             TeamEgoist.Add(ego);
         }
         public static bool IsEnable => playerIdList.Count > 0;
-        public static void ApplyKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
+        public static void ApplyKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown;
         public static void OverrideCustomWinner()
         {
             foreach (var id in playerIdList)
