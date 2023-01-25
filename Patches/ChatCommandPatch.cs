278d95232c79678d1e2de6682a07cfcc11146f2c
tanakanira0118 <github@tanakanira.com>
2023-01-25 17:32:31 +0900

    /killlogコマンドを追加

--- Patches/ChatCommandPatch.cs
+++ Patches/ChatCommandPatch.cs
@@ -65,6 +65,12 @@ namespace TownOfHost
                         Utils.ShowLastResult();
                         break;
 
+                    case "/kl":
+                    case "/killlog":
+                        canceled = true;
+                        Utils.ShowKillLog();
+                        break;
+
                     case "/r":
                     case "/rename":
                         canceled = true;
@@ -355,6 +361,11 @@ namespace TownOfHost
                 case "/l":
                 case "/lastresult":
                     Utils.ShowLastResult(player.PlayerId);
+                    break;
+
+                case "/kl":
+                case "/killlog":
+                    Utils.ShowKillLog(player.PlayerId);
                     break;
 
                 case "/n":
