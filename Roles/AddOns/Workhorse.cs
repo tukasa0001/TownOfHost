64086cea903e19f3f76f93f604db7c40205bb934
Masami <103577031+Masami4711@users.noreply.github.com>
2023-02-01 13:50:54 +0900

    GetStringをGetRoleNameに

--- Roles/AddOns/Workhorse.cs
+++ Roles/AddOns/Workhorse.cs
@@ -15,7 +15,7 @@ namespace TownOfHost
         {
             SetupRoleOptions(Id, TabGroup.Addons, CustomRoles.Workhorse);
             AssignOnlyToCrewmate = BooleanOptionItem.Create(Id + 10, "AssignOnlyTo%role%", true, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Workhorse]);
-            AssignOnlyToCrewmate.ReplacementDictionary = new Dictionary<string, string> { { "%role%", Utils.ColorString(Palette.CrewmateBlue, Translator.GetString("Crewmate")) } };
+            AssignOnlyToCrewmate.ReplacementDictionary = new Dictionary<string, string> { { "%role%", Utils.ColorString(Palette.CrewmateBlue, Utils.GetRoleName(CustomRoles.Crewmate)) } };
             NumLongTasks = IntegerOptionItem.Create(Id + 11, "WorkhorseNumLongTasks", new(0, 5, 1), 1, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Workhorse])
                 .SetValueFormat(OptionFormat.Pieces);
             NumShortTasks = IntegerOptionItem.Create(Id + 12, "WorkhorseNumShortTasks", new(0, 5, 1), 1, TabGroup.Addons, false).SetParent(CustomRoleSpawnChances[CustomRoles.Workhorse])
