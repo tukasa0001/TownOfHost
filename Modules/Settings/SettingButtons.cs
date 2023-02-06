e629dae3eae577a775c190fdcfa4bcee40d49b80
Dekokiyo <dekobokokiyomori0817@gmail.com>
2023-02-06 15:46:09 +0900

    先頭大文字に変更

--- Modules/Settings/SettingButtons.cs
+++ Modules/Settings/SettingButtons.cs
@@ -7,19 +7,19 @@ public static class SettingButtons
 {
     // Load
     public static GameObject LoadButton;
-    public static SpriteRenderer loadRenderer;
-    public static PassiveButton passiveLoadButton;
-    public static BoxCollider2D loadCollider2D;
-    public static AspectPosition loadAspect;
-    public static ButtonRolloverHandler loadButtonRolloverHandler;
+    public static SpriteRenderer LoadRenderer;
+    public static PassiveButton PassiveLoadButton;
+    public static BoxCollider2D LoadCollider2D;
+    public static AspectPosition LoadAspect;
+    public static ButtonRolloverHandler LoadButtonRolloverHandler;
 
     // Save
     public static GameObject SaveButton;
-    public static SpriteRenderer saveRenderer;
-    public static PassiveButton passiveSaveButton;
-    public static BoxCollider2D saveCollider2D;
-    public static AspectPosition saveAspect;
-    public static ButtonRolloverHandler saveButtonRolloverHandler;
+    public static SpriteRenderer SaveRenderer;
+    public static PassiveButton PassiveSaveButton;
+    public static BoxCollider2D SaveCollider2D;
+    public static AspectPosition SaveAspect;
+    public static ButtonRolloverHandler SaveButtonRolloverHandler;
 
     public static void SetButtons()
     {
@@ -30,31 +30,31 @@ public static class SettingButtons
             name = "LoadButton",
         };
         LoadButton.transform.parent = HudManager.Instance.transform;
-        loadRenderer = LoadButton.AddComponent<SpriteRenderer>();
-        loadRenderer.sprite = Utils.LoadSprite("TownOfHost.Resources.Load.png", 300f);
-        passiveLoadButton = LoadButton.AddComponent<PassiveButton>();
-        passiveLoadButton.OnClick = new();
-        passiveLoadButton.OnClick.AddListener((System.Action)delegate
+        LoadRenderer = LoadButton.AddComponent<SpriteRenderer>();
+        LoadRenderer.sprite = Utils.LoadSprite("TownOfHost.Resources.Load.png", 300f);
+        PassiveLoadButton = LoadButton.AddComponent<PassiveButton>();
+        PassiveLoadButton.OnClick = new();
+        PassiveLoadButton.OnClick.AddListener((System.Action)delegate
         {
             LoadSettings.Load();
         });
-        passiveLoadButton.OnMouseOver = new();
-        passiveLoadButton.OnMouseOver.AddListener((UnityAction)delegate
+        PassiveLoadButton.OnMouseOver = new();
+        PassiveLoadButton.OnMouseOver.AddListener((UnityAction)delegate
         {
-            loadRenderer.color = Palette.AcceptedGreen;
+            LoadRenderer.color = Palette.AcceptedGreen;
         });
-        passiveLoadButton.OnMouseOut = new();
-        passiveLoadButton.OnMouseOut.AddListener((UnityAction)delegate
+        PassiveLoadButton.OnMouseOut = new();
+        PassiveLoadButton.OnMouseOut.AddListener((UnityAction)delegate
         {
-            loadRenderer.color = Palette.White;
+            LoadRenderer.color = Palette.White;
         });
-        loadCollider2D = LoadButton.AddComponent<BoxCollider2D>();
-        loadAspect = LoadButton.AddComponent<AspectPosition>();
-        loadAspect.Alignment = AspectPosition.EdgeAlignments.LeftBottom;
-        loadAspect.DistanceFromEdge = new(0.25f, 0.75f, -400);
-        loadAspect.updateAlways = true;
-        loadButtonRolloverHandler = LoadButton.AddComponent<ButtonRolloverHandler>();
-        loadButtonRolloverHandler.HoverSound = HudManager.Instance.Chat.ChatButton.GetComponent<ButtonRolloverHandler>().HoverSound;
+        LoadCollider2D = LoadButton.AddComponent<BoxCollider2D>();
+        LoadAspect = LoadButton.AddComponent<AspectPosition>();
+        LoadAspect.Alignment = AspectPosition.EdgeAlignments.LeftBottom;
+        LoadAspect.DistanceFromEdge = new(0.25f, 0.75f, -400);
+        LoadAspect.updateAlways = true;
+        LoadButtonRolloverHandler = LoadButton.AddComponent<ButtonRolloverHandler>();
+        LoadButtonRolloverHandler.HoverSound = HudManager.Instance.Chat.ChatButton.GetComponent<ButtonRolloverHandler>().HoverSound;
 
         SaveButton = new()
         {
@@ -63,31 +63,31 @@ public static class SettingButtons
             name = "SaveButton",
         };
         SaveButton.transform.parent = HudManager.Instance.transform;
-        saveRenderer = SaveButton.AddComponent<SpriteRenderer>();
-        saveRenderer.sprite = Utils.LoadSprite("TownOfHost.Resources.Save.png", 300f);
-        passiveSaveButton = SaveButton.AddComponent<PassiveButton>();
-        passiveSaveButton.OnClick = new();
-        passiveSaveButton.OnClick.AddListener((UnityAction)delegate
+        SaveRenderer = SaveButton.AddComponent<SpriteRenderer>();
+        SaveRenderer.sprite = Utils.LoadSprite("TownOfHost.Resources.Save.png", 300f);
+        PassiveSaveButton = SaveButton.AddComponent<PassiveButton>();
+        PassiveSaveButton.OnClick = new();
+        PassiveSaveButton.OnClick.AddListener((UnityAction)delegate
         {
             SaveSettings.Save();
         });
-        passiveSaveButton.OnMouseOver = new();
-        passiveSaveButton.OnMouseOver.AddListener((UnityAction)delegate
+        PassiveSaveButton.OnMouseOver = new();
+        PassiveSaveButton.OnMouseOver.AddListener((UnityAction)delegate
         {
-            saveRenderer.color = Palette.AcceptedGreen;
+            SaveRenderer.color = Palette.AcceptedGreen;
         });
-        passiveSaveButton.OnMouseOut = new();
-        passiveSaveButton.OnMouseOut.AddListener((UnityAction)delegate
+        PassiveSaveButton.OnMouseOut = new();
+        PassiveSaveButton.OnMouseOut.AddListener((UnityAction)delegate
         {
-            saveRenderer.color = Palette.White;
+            SaveRenderer.color = Palette.White;
         });
-        saveCollider2D = SaveButton.AddComponent<BoxCollider2D>();
-        saveAspect = SaveButton.AddComponent<AspectPosition>();
-        saveAspect.Alignment = AspectPosition.EdgeAlignments.LeftBottom;
-        saveAspect.DistanceFromEdge = new(0.25f, 0.3f, -400);
-        saveAspect.updateAlways = true;
-        saveButtonRolloverHandler = SaveButton.AddComponent<ButtonRolloverHandler>();
-        saveButtonRolloverHandler.HoverSound = HudManager.Instance.Chat.ChatButton.GetComponent<ButtonRolloverHandler>().HoverSound;
+        SaveCollider2D = SaveButton.AddComponent<BoxCollider2D>();
+        SaveAspect = SaveButton.AddComponent<AspectPosition>();
+        SaveAspect.Alignment = AspectPosition.EdgeAlignments.LeftBottom;
+        SaveAspect.DistanceFromEdge = new(0.25f, 0.3f, -400);
+        SaveAspect.updateAlways = true;
+        SaveButtonRolloverHandler = SaveButton.AddComponent<ButtonRolloverHandler>();
+        SaveButtonRolloverHandler.HoverSound = HudManager.Instance.Chat.ChatButton.GetComponent<ButtonRolloverHandler>().HoverSound;
     }
 
     public static void ButtonsVisible(bool active)
