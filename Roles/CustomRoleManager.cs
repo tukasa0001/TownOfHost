5509287f43dcb3dfe60bf0431db791fb1a82a5dd
そうくん <daoyecangma@gmail.com>
2023-02-12 16:37:07 +0900

    CustomRoleManagerを追加

---
+++ Roles/CustomRoleManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace TownOfHost.Roles;
+
+public static class CustomRoleManager
+{
+    public static List<RoleBase> AllRoles = new(Enum.GetValues(typeof(CustomRoles)).Length);
+
+    public static void Initialize()
+    {
+        AllRoles.Do(role => role.IsEnable = role.RoleName.IsEnable());
+    }
+    public static RoleBase Get(this List<RoleBase> roleBase, CustomRoles role) => roleBase.ToArray().Where(roleClass => roleClass.RoleName == role).FirstOrDefault();
+    public static void Do(this List<RoleBase> roleBase, Action<RoleBase> action) => roleBase.ToArray().Do(action);
+}
