1f606fd42488bd46c9cbe2ad4feef0c78fd49759
そうくん <daoyecangma@gmail.com>
2023-02-12 16:34:08 +0900

    フィールドとコンストラクタを宣言

--- Roles/RoleBase.cs
+++ Roles/RoleBase.cs
@@ -1,6 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+using static TownOfHost.Options;
 
 namespace TownOfHost.Roles;
 
 public abstract class RoleBase
 {
+    public List<byte> PlayerIdList;
+    public CustomRoles RoleName;
+    public RoleType CustomRoleType;
+    public Color32 RoleColor;
+    public string RoleColorCode;
+    public int ConfigId;
+    public TabGroup Tab;
+    public OptionItem RoleOption => CustomRoleSpawnChances[RoleName];
+    public bool IsEnable = false;
+
+
+    public RoleBase(
+        CustomRoles roleName,
+        RoleType type,
+        int configId,
+        string colorCode = "",
+        TabGroup tab = TabGroup.MainSettings
+    )
+    {
+    }
 }
