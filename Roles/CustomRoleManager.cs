934202d58506ac4c7d1af8255d750620b1b89af0
そうくん <daoyecangma@gmail.com>
2023-02-14 19:37:44 +0900

    Initializeを移動

--- Roles/CustomRoleManager.cs
+++ Roles/CustomRoleManager.cs
@@ -10,15 +10,15 @@ public static class CustomRoleManager
     public static List<SimpleRoleInfo> AllRolesInfo = new(Enum.GetValues(typeof(CustomRoles)).Length);
     public static List<RoleBase> AllActiveRoles = new(Enum.GetValues(typeof(CustomRoles)).Length);
 
-    public static void Initialize()
-    {
-        AllRolesInfo.Do(role => role.IsEnable = role.RoleName.IsEnable());
-        AllActiveRoles.Clear();
-    }
     public static SimpleRoleInfo GetRoleInfo(this CustomRoles role) => AllRolesInfo.ToArray().Where(info => info.RoleName == role).FirstOrDefault();
     public static RoleBase GetRoleClass(this PlayerControl player) => GetByPlayerId(player.PlayerId);
     public static RoleBase GetByPlayerId(byte playerId) => AllActiveRoles.ToArray().Where(roleClass => roleClass.Player.PlayerId == playerId).FirstOrDefault();
     public static void Do<T>(this List<T> roleBase, Action<T> action) => roleBase.ToArray().Do(action);
     // == CheckMurder関連処理 ==
     // ==/CheckMurder関連処理 ==
+    public static void Initialize()
+    {
+        AllRolesInfo.Do(role => role.IsEnable = role.RoleName.IsEnable());
+        AllActiveRoles.Clear();
+    }
 }
