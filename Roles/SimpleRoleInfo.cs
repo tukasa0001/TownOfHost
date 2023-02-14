b71771acb486c45078fd9ee0e600e7055b9e1ac4
そうくん <daoyecangma@gmail.com>
2023-02-14 18:44:08 +0900

    SimpleRoleinfoクラス作成

---
+++ Roles/SimpleRoleInfo.cs
@@ -0,0 +1,44 @@
+
+using static TownOfHost.Options;
+
+namespace TownOfHost.Roles;
+
+public class SimpleRoleInfo : RoleInfoBase
+{
+    public int ConfigId;
+    public TabGroup Tab;
+    public OptionItem RoleOption => CustomRoleSpawnChances[RoleName];
+    public bool IsEnable = false;
+    public OptionCreatorDelegate OptionCreator;
+
+    public SimpleRoleInfo(
+        CustomRoles roleName,
+        RoleType type,
+        int configId,
+        string colorCode = "",
+        OptionCreatorDelegate optionCreator = null,
+        TabGroup tab = TabGroup.MainSettings
+    ) :
+    base(
+        roleName,
+        type,
+        colorCode
+    )
+    {
+        ConfigId = configId;
+        OptionCreator = optionCreator;
+
+        if (tab == TabGroup.MainSettings)
+            tab = CustomRoleType switch
+            {
+                RoleType.Impostor => TabGroup.ImpostorRoles,
+                RoleType.Madmate => TabGroup.ImpostorRoles,
+                RoleType.Crewmate => TabGroup.CrewmateRoles,
+                RoleType.Neutral => TabGroup.NeutralRoles,
+                _ => tab
+            };
+        Tab = tab;
+
+        CustomRoleManager.AllRolesInfo.Add(this);
+    }
+}
