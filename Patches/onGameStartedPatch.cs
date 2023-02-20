b2ccf6d85c7197150307806b1f14f25c3819a03f
KARPED1EM <leever.zzz@gmail.com>
2023-02-20 08:41:46 +0800

    侦探不会成为胆小鬼

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -783,6 +783,7 @@ namespace TownOfHost
                 if (role is CustomRoles.Bewilder && pc.GetCustomRole().IsImpostor()) continue;
                 if (role is CustomRoles.Ntr && pc.Is(CustomRoles.Lovers)) continue;
                 if (role is CustomRoles.Madmate && (pc.GetCustomRole().IsImpostorTeam() || pc.Is(CustomRoles.Needy) || pc.Is(CustomRoles.Snitch) || pc.Is(CustomRoles.Sheriff) || pc.Is(CustomRoles.Mayor) || pc.Is(CustomRoles.NiceGuesser))) continue;
+                if (role is CustomRoles.Oblivious && pc.Is(CustomRoles.Detective)) continue;
                 allPlayers.Add(pc);
             }
             var rd = IRandom.Instance;
