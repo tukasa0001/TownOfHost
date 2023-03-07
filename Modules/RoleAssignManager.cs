3a79053217a7d9eca009a96ab85e80e8ef503fa1
Masami <103577031+Masami4711@users.noreply.github.com>
2023-03-07 18:12:15 +0900

    アサイン設定の初期値を0に

--- Modules/RoleAssignManager.cs
+++ Modules/RoleAssignManager.cs
@@ -24,26 +24,26 @@ namespace TownOfHost.Modules
 
         public static void SetupCustomOption()
         {
-            ImpostorMin = IntegerOptionItem.Create(idStart, "ImpostorRolesMin", new(0, 3, 1), 3, TabGroup.ImpostorRoles, false)
+            ImpostorMin = IntegerOptionItem.Create(idStart, "ImpostorRolesMin", new(0, 3, 1), 0, TabGroup.ImpostorRoles, false)
                 .SetHeader(true)
                 .SetValueFormat(OptionFormat.Players);
-            ImpostorMax = IntegerOptionItem.Create(idStart + 1, "ImpostorRolesMax", new(0, 3, 1), 3, TabGroup.ImpostorRoles, false)
+            ImpostorMax = IntegerOptionItem.Create(idStart + 1, "ImpostorRolesMax", new(0, 3, 1), 0, TabGroup.ImpostorRoles, false)
                 .SetValueFormat(OptionFormat.Players);
-            MadmateMin = IntegerOptionItem.Create(idStart + 6, "MadRolesMin", new(0, 15, 1), 15, TabGroup.ImpostorRoles, false)
+            MadmateMin = IntegerOptionItem.Create(idStart + 6, "MadRolesMin", new(0, 15, 1), 0, TabGroup.ImpostorRoles, false)
                 .SetValueFormat(OptionFormat.Players);
-            MadmateMax = IntegerOptionItem.Create(idStart + 7, "MadRolesMax", new(0, 15, 1), 15, TabGroup.ImpostorRoles, false)
+            MadmateMax = IntegerOptionItem.Create(idStart + 7, "MadRolesMax", new(0, 15, 1), 0, TabGroup.ImpostorRoles, false)
                 .SetValueFormat(OptionFormat.Players);
 
-            CrewmateMin = IntegerOptionItem.Create(idStart + 2, "CrewmateRolesMin", new(0, 15, 1), 15, TabGroup.CrewmateRoles, false)
+            CrewmateMin = IntegerOptionItem.Create(idStart + 2, "CrewmateRolesMin", new(0, 15, 1), 0, TabGroup.CrewmateRoles, false)
                 .SetHeader(true)
                 .SetValueFormat(OptionFormat.Players);
-            CrewmateMax = IntegerOptionItem.Create(idStart + 3, "CrewmateRolesMax", new(0, 15, 1), 15, TabGroup.CrewmateRoles, false)
+            CrewmateMax = IntegerOptionItem.Create(idStart + 3, "CrewmateRolesMax", new(0, 15, 1), 0, TabGroup.CrewmateRoles, false)
                 .SetValueFormat(OptionFormat.Players);
 
-            NeutralMin = IntegerOptionItem.Create(idStart + 4, "NeutralRolesMin", new(0, 15, 1), 15, TabGroup.NeutralRoles, false)
+            NeutralMin = IntegerOptionItem.Create(idStart + 4, "NeutralRolesMin", new(0, 15, 1), 0, TabGroup.NeutralRoles, false)
                 .SetHeader(true)
                 .SetValueFormat(OptionFormat.Players);
-            NeutralMax = IntegerOptionItem.Create(idStart + 5, "NeutralRolesMax", new(0, 15, 1), 15, TabGroup.NeutralRoles, false)
+            NeutralMax = IntegerOptionItem.Create(idStart + 5, "NeutralRolesMax", new(0, 15, 1), 0, TabGroup.NeutralRoles, false)
                 .SetValueFormat(OptionFormat.Players);
         }
         public static void SelectAssignRoles()
