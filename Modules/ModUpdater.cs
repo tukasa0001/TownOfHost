aace5a62fad6799b203b5336cb94c84ef3e17100
KARPED1EM <leever.zzz@gmail.com>
2023-03-11 12:39:21 +0800

    Remove Force Version Check Out of China

--- Modules/ModUpdater.cs
+++ Modules/ModUpdater.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using System;
+using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Reflection;
@@ -141,8 +142,16 @@ public class ModUpdater
         }
         catch (Exception ex)
         {
-            isChecked = false;
-            isBroken = true;
+            if (CultureInfo.CurrentCulture.Name == "zh-CN")
+            {
+                isChecked = false;
+                isBroken = true;
+            }
+            else
+            {
+                isChecked = true;
+                isBroken = false;
+            }
             Logger.Error($"检查更新时发生错误\n{ex}", "CheckRelease", false);
             return Task.FromResult(false);
         }
