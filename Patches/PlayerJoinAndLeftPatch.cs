32fdce19dc52e6579d3062675385a9516433f66a
Babydino223 <84541716+Babydino223@users.noreply.github.com>
2023-03-13 17:06:29 -0400

    Update PlayerJoinAndLeftPatch.cs
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- Patches/PlayerJoinAndLeftPatch.cs
+++ Patches/PlayerJoinAndLeftPatch.cs
@@ -41,6 +41,7 @@ namespace TownOfHost
         public static void Prefix(InnerNetClient __instance, DisconnectReasons reason, string stringReason)
         {
             Logger.Info($"切断(理由:{reason}:{stringReason}, ping:{__instance.Ping})", "Session");
+            versionChecked = false;
         }
     }
     [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.OnPlayerJoined))]
