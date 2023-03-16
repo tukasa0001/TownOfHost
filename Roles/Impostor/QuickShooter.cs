f03a45dcf3a04eb233d983cb8ce29dd4e4d9dab9
KARPED1EM <leever.zzz@gmail.com>
2023-03-16 15:59:06 +0800

    Fix QuickShooter OptionFormat

--- Roles/Impostor/QuickShooter.cs
+++ Roles/Impostor/QuickShooter.cs
@@ -17,12 +17,11 @@ namespace TOHE.Roles.Impostor
         public static void SetupCustomOption()
         {
             Options.SetupRoleOptions(Id, TabGroup.ImpostorRoles, CustomRoles.QuickShooter);
-            KillCooldown = FloatOptionItem.Create(Id + 10, "KillCooldown", new(0f, 990f, 1f), 15f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.QuickShooter])
+            KillCooldown = FloatOptionItem.Create(Id + 10, "KillCooldown", new(0f, 990f, 1f), 35f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.QuickShooter])
                 .SetValueFormat(OptionFormat.Seconds);
-            MeetingReserved = IntegerOptionItem.Create(Id + 12, "MeetingReserved", new(0, 100, 1), 1, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.QuickShooter])
-                .SetValueFormat(OptionFormat.Times);
-            ShapeshiftCooldown = FloatOptionItem.Create(Id + 14, "QuickShooterShapeshiftCooldown", new(5f, 990f, 1f), 15f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.QuickShooter])
+            ShapeshiftCooldown = FloatOptionItem.Create(Id + 12, "QuickShooterShapeshiftCooldown", new(5f, 990f, 1f), 15f, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.QuickShooter])
                 .SetValueFormat(OptionFormat.Seconds);
+            MeetingReserved = IntegerOptionItem.Create(Id + 14, "MeetingReserved", new(0, 100, 1), 2, TabGroup.ImpostorRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.QuickShooter]);
         }
         public static void Init()
         {
