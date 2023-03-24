0d431f03941ad09ff6d418ec0092caeb7443db49
KARPED1EM <leever.zzz@gmail.com>
2023-03-24 11:18:39 +0800

    Fix FixKillCooldownTask

--- Patches/IntroPatch.cs
+++ Patches/IntroPatch.cs
@@ -280,7 +280,7 @@ class IntroCutsceneDestroyPatch
                 if (Options.FixFirstKillCooldown.GetBool())
                     new LateTask(() =>
                     {
-                        Main.AllPlayerControls.Do(pc => pc.SetKillCooldown(Main.AllPlayerKillCooldown[pc.PlayerId] - 2f));
+                        Main.AllPlayerControls.Where(x => (Main.AllPlayerKillCooldown[x.PlayerId] - 2f) > 0f).Do(pc => pc.SetKillCooldown(Main.AllPlayerKillCooldown[pc.PlayerId] - 2f));
                     }, 2f, "FixKillCooldownTask");
             }
             new LateTask(() => Main.AllPlayerControls.Do(pc => pc.RpcSetRoleDesync(RoleTypes.Shapeshifter, -3)), 2f, "SetImpostorForServer");
