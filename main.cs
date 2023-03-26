216764a9af311e2a82a6cfaff25c81613034ed0a
KARPED1EM <leever.zzz@gmail.com>
2023-03-26 19:33:42 +0800

    Update Version

--- main.cs
+++ main.cs
@@ -36,7 +36,7 @@ public class Main : BasePlugin
     public static readonly string BANNEDWORDS_FILE_PATH = "./TOHE_DATA/BanWords.txt";
     public const string PluginGuid = "com.karped1em.townofhostedited";
     public const string PluginVersion = "2.2.2";
-    public const int PluginCreate = 14;
+    public const int PluginCreate = 15;
     public Harmony Harmony { get; } = new Harmony(PluginGuid);
     public static Version version = Version.Parse(PluginVersion);
     public static BepInEx.Logging.ManualLogSource Logger;
