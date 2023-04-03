2c899cba41513d1740e62587d0c1a0642932b11f
そうくん <daoyecangma@gmail.com>
2023-04-03 22:58:25 +0900

    指摘箇所修正

--- Roles/Crewmate/Mayor.cs
+++ Roles/Crewmate/Mayor.cs
@@ -27,7 +27,7 @@ public sealed class Mayor : RoleBase
         HasPortableButton = OptionHasPortableButton.GetBool();
         NumOfUseButton = OptionNumOfUseButton.GetInt();
 
-        LeftButtonCount = Main.NormalOptions.NumEmergencyMeetings + NumOfUseButton;
+        LeftButtonCount = NumOfUseButton;
     }
 
     private static OptionItem OptionAdditionalVote;
@@ -61,13 +61,13 @@ public sealed class Mayor : RoleBase
             : opt.GetInt(Int32OptionNames.EmergencyCooldown);
         AURoleOptions.EngineerInVentMaxTime = 1;
     }
-    public override bool OnReportDeadBody(PlayerControl reporter, GameData.PlayerInfo target)
+    /*public override bool OnReportDeadBody(PlayerControl reporter, GameData.PlayerInfo target)
     {
         if (reporter.Is(CustomRoles.Mayor) && target == null) //ボタン
             LeftButtonCount--;
 
         return true;
-    }
+    }*/
     public override bool OnEnterVent(PlayerPhysics physics, int ventId)
     {
         if (LeftButtonCount > 0)
@@ -75,6 +75,7 @@ public sealed class Mayor : RoleBase
             var user = physics.myPlayer;
             physics.RpcBootFromVent(ventId);
             user?.ReportDeadBody(null);
+            LeftButtonCount--;
 
             return false;
         }
