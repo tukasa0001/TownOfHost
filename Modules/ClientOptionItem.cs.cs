a45bc0fb1a28ab9421382e03c541b9de95e4833f
KARPED1EM <leever.zzz@gmail.com>
2023-04-06 16:30:46 +0800

    TOHE Options: https://github.com/tukasa0001/TownOfHost/pull/1265

---
+++ Modules/ClientOptionItem.cs.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using BepInEx.Configuration;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace TOHE;
+
+//来源：https://github.com/tukasa0001/TownOfHost/pull/1265/files
+public class ClientOptionItem
+{
+    public ConfigEntry<bool> Config;
+    public ToggleButtonBehaviour ToggleButton;
+
+    public static SpriteRenderer CustomBackground;
+    private static int numOptions = 0;
+
+    private ClientOptionItem(
+        string name,
+        ConfigEntry<bool> config,
+        OptionsMenuBehaviour optionsMenuBehaviour,
+        Action additionalOnClickAction = null)
+    {
+        try
+        {
+            Config = config;
+
+            var mouseMoveToggle = optionsMenuBehaviour.DisableMouseMovement;
+
+            // 1つ目のボタンの生成時に背景も生成
+            if (CustomBackground == null)
+            {
+                numOptions = 0;
+                CustomBackground = Object.Instantiate(optionsMenuBehaviour.Background, optionsMenuBehaviour.transform);
+                CustomBackground.name = "CustomBackground";
+                CustomBackground.transform.localScale = new(0.9f, 0.9f, 1f);
+                CustomBackground.transform.localPosition += Vector3.back * 8;
+                CustomBackground.gameObject.SetActive(false);
+
+                var closeButton = Object.Instantiate(mouseMoveToggle, CustomBackground.transform);
+                closeButton.transform.localPosition = new(1.3f, -2.3f, -6f);
+                closeButton.name = "Close";
+                closeButton.Text.text = Translator.GetString("Close");
+                closeButton.Background.color = Palette.DisabledGrey;
+                var closePassiveButton = closeButton.GetComponent<PassiveButton>();
+                closePassiveButton.OnClick = new();
+                closePassiveButton.OnClick.AddListener(new Action(() =>
+                {
+                    CustomBackground.gameObject.SetActive(false);
+                }));
+
+                UiElement[] selectableButtons = optionsMenuBehaviour.ControllerSelectable.ToArray();
+                PassiveButton leaveButton = null;
+                PassiveButton returnButton = null;
+                for (int i = 0; i < selectableButtons.Length; i++)
+                {
+                    var button = selectableButtons[i];
+                    if (button == null)
+                    {
+                        continue;
+                    }
+
+                    if (button.name == "LeaveGameButton")
+                    {
+                        leaveButton = button.GetComponent<PassiveButton>();
+                    }
+                    else if (button.name == "ReturnToGameButton")
+                    {
+                        returnButton = button.GetComponent<PassiveButton>();
+                    }
+                }
+                var generalTab = mouseMoveToggle.transform.parent.parent.parent;
+
+                var modOptionsButton = Object.Instantiate(mouseMoveToggle, generalTab);
+                modOptionsButton.transform.localPosition = leaveButton?.transform?.localPosition ?? new(0f, -2.4f, 1f);
+                modOptionsButton.name = "TOHEOptions";
+                modOptionsButton.Text.text = Translator.GetString("TOHEOptions");
+                modOptionsButton.Background.color = new Color32(0x00, 0xbf, 0xff, 0xff);
+                var modOptionsPassiveButton = modOptionsButton.GetComponent<PassiveButton>();
+                modOptionsPassiveButton.OnClick = new();
+                modOptionsPassiveButton.OnClick.AddListener(new Action(() =>
+                {
+                    CustomBackground.gameObject.SetActive(true);
+                }));
+
+                if (leaveButton != null)
+                {
+                    leaveButton.transform.localPosition = new(-1.35f, -2.411f, -1f);
+                }
+                if (returnButton != null)
+                {
+                    returnButton.transform.localPosition = new(1.35f, -2.411f, -1f);
+                }
+            }
+
+            // ボタン生成
+            ToggleButton = Object.Instantiate(mouseMoveToggle, CustomBackground.transform);
+            ToggleButton.transform.localPosition = new Vector3(
+                // 現在のオプション数を基に位置を計算
+                numOptions % 2 == 0 ? -1.3f : 1.3f,
+                2.2f - (0.5f * (numOptions / 2)),
+                -6f);
+            ToggleButton.name = name;
+            ToggleButton.Text.text = Translator.GetString(name);
+            var passiveButton = ToggleButton.GetComponent<PassiveButton>();
+            passiveButton.OnClick = new();
+            passiveButton.OnClick.AddListener(new Action(() =>
+            {
+                config.Value = !config.Value;
+                UpdateToggle();
+                additionalOnClickAction?.Invoke();
+            }));
+            UpdateToggle();
+        }
+        finally
+        {
+            numOptions++;
+        }
+    }
+
+    public static ClientOptionItem Create(
+        string name,
+        ConfigEntry<bool> config,
+        OptionsMenuBehaviour optionsMenuBehaviour,
+        Action additionalOnClickAction = null)
+    {
+        return new(name, config, optionsMenuBehaviour, additionalOnClickAction);
+    }
+
+    public void UpdateToggle()
+    {
+        if (ToggleButton == null)
+        {
+            return;
+        }
+
+        var color = Config.Value ? Color.green : Color.red;
+        ToggleButton.Background.color = color;
+        if (ToggleButton.Rollover != null)
+        {
+            ToggleButton.Rollover.ChangeOutColor(color);
+        }
+    }
+}
--- Patches/ClientOptionsPatch.cs
+++ Patches/ClientOptionsPatch.cs
@@ -1,69 +1,21 @@
 using HarmonyLib;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace TOHE;
 
+//À´Ô´£ºhttps://github.com/tukasa0001/TownOfHost/pull/1265/files
 [HarmonyPatch(typeof(OptionsMenuBehaviour), nameof(OptionsMenuBehaviour.Start))]
-internal class OptionsMenuBehaviourStartPatch
+public static class OptionsMenuBehaviourStartPatch
 {
-    private static Vector3? origin;
-    private static ToggleButtonBehaviour UnlockFPS;
-    private static ToggleButtonBehaviour AutoStart;
-    private static ToggleButtonBehaviour DisableTOHE;
-    public static float xOffset = 1.75f;
-    public static float yOffset = -0.25f;
-
-    private static void UpdateToggle(ToggleButtonBehaviour button, string text, bool on)
-    {
-        if (button == null || button.gameObject == null) return;
-
-        Color color = on ? new Color(0f, 1f, 0.16470589f, 1f) : Color.white;
-        button.Background.color = color;
-        button.Text.text = $"{text}{(on ? Translator.GetString("turnOn") : Translator.GetString("turnOff"))}";
-        if (button.Rollover) button.Rollover.ChangeOutColor(color);
-    }
-    private static ToggleButtonBehaviour CreateCustomToggle(string text, bool on, Vector3 offset, UnityEngine.Events.UnityAction onClick, OptionsMenuBehaviour __instance, ToggleButtonBehaviour tb)
-    {
-        if (tb != null)
-        {
-            var button = Object.Instantiate(tb, tb.transform.parent);
-            button.transform.localPosition = (origin ?? Vector3.zero) + offset;
-            PassiveButton passiveButton = button.GetComponent<PassiveButton>();
-            passiveButton.OnClick = new Button.ButtonClickedEvent();
-            passiveButton.OnClick.AddListener(onClick);
-            UpdateToggle(button, text, on);
-
-            return button;
-        }
-        return null;
-    }
+    private static ClientOptionItem UnlockFPS;
+    private static ClientOptionItem AutoStart;
+    private static ClientOptionItem DisableTOHE;
 
     public static void Postfix(OptionsMenuBehaviour __instance)
     {
-        if (__instance.CensorChatButton != null)
+        if (__instance.DisableMouseMovement == null)
         {
-            if (origin == null) origin = __instance.CensorChatButton.transform.localPosition;
-            __instance.CensorChatButton.transform.localPosition = origin.Value + Vector3.left * 0.375f + Vector3.up * 0.08f;
-            __instance.CensorChatButton.transform.localScale = Vector3.one * 0.7f;
-        }
-        if (__instance.EnableFriendInvitesButton != null)
-        {
-            if (origin == null) origin = __instance.EnableFriendInvitesButton.transform.localPosition;
-            __instance.EnableFriendInvitesButton.transform.localPosition = origin.Value + Vector3.right * 3.125f + Vector3.up * 0.08f;
-            __instance.EnableFriendInvitesButton.transform.localScale = Vector3.one * 0.7f;
-        }
-        if (__instance.ColorBlindButton != null)
-        {
-            if (origin == null) origin = __instance.ColorBlindButton.transform.localPosition;
-            __instance.ColorBlindButton.transform.localPosition = origin.Value + Vector3.right * 3.125f + Vector3.up * 0.74f;
-            __instance.ColorBlindButton.transform.localScale = Vector3.one * 0.7f;
-        }
-        if (__instance.StreamerModeButton != null)
-        {
-            if (origin == null) origin = __instance.StreamerModeButton.transform.localPosition;
-            __instance.StreamerModeButton.transform.localPosition = origin.Value + Vector3.right * 1.375f + Vector3.up * 1.071f;
-            __instance.StreamerModeButton.transform.localScale = Vector3.one * 0.7f;
+            return;
         }
 
         Main.DisableTOHE.Value = false;
@@ -73,26 +25,20 @@ internal class OptionsMenuBehaviourStartPatch
             Main.SetAutoStartToDisable = true;
         }
 
-        if (DisableTOHE == null || DisableTOHE?.gameObject == null)
+        if (UnlockFPS == null || UnlockFPS.ToggleButton == null)
         {
-            DisableTOHE = CreateCustomToggle(Translator.GetString("DisableTOHE") + ": ", Main.DisableTOHE.Value, new Vector3(-0.375f, yOffset, 0), (UnityEngine.Events.UnityAction)DisableTOHEButtonToggle, __instance, __instance.CensorChatButton);
-
-            void DisableTOHEButtonToggle()
+            UnlockFPS = ClientOptionItem.Create("UnlockFPS", Main.UnlockFPS, __instance, UnlockFPSButtonToggle);
+            static void UnlockFPSButtonToggle()
             {
-                Main.DisableTOHE.Value = !Main.DisableTOHE.Value;
-                UpdateToggle(DisableTOHE, Translator.GetString("DisableTOHE") + ": ", Main.DisableTOHE.Value);
-                Harmony.UnpatchAll();
-                Main.Instance.Unload();
+                Application.targetFrameRate = Main.UnlockFPS.Value ? 165 : 60;
+                Logger.SendInGame(string.Format(Translator.GetString("FPSSetTo"), Application.targetFrameRate));
             }
         }
-        if (AutoStart == null || AutoStart.gameObject == null)
+        if (AutoStart == null || AutoStart.ToggleButton == null)
         {
-            AutoStart = CreateCustomToggle(Translator.GetString("AutoStart") + ": ", Main.AutoStart.Value, new Vector3(1.375f, yOffset, 0), (UnityEngine.Events.UnityAction)AutoStartButtonToggle, __instance, __instance.CensorChatButton);
-
-            void AutoStartButtonToggle()
+            AutoStart = ClientOptionItem.Create("AutoStart", Main.AutoStart, __instance, AutoStartButtonToggle);
+            static void AutoStartButtonToggle()
             {
-                Main.AutoStart.Value = !Main.AutoStart.Value;
-                UpdateToggle(AutoStart, Translator.GetString("AutoStart") + ": ", Main.AutoStart.Value);
                 if (Main.AutoStart.Value == false && GameStates.IsCountDown)
                 {
                     GameStartManager.Instance.ResetStartState();
@@ -100,17 +46,26 @@ internal class OptionsMenuBehaviourStartPatch
                 }
             }
         }
-        if (UnlockFPS == null || UnlockFPS.gameObject == null)
+        if (DisableTOHE == null || DisableTOHE.ToggleButton == null)
         {
-            UnlockFPS = CreateCustomToggle(Translator.GetString("UnlockFPS") + ": ", Main.UnlockFPS.Value, new Vector3(1.375f, yOffset - 0.33f, 0), (UnityEngine.Events.UnityAction)UnlockFPSButtonToggle, __instance, __instance.CensorChatButton);
-
-            void UnlockFPSButtonToggle()
+            DisableTOHE = ClientOptionItem.Create("DisableTOHE", Main.DisableTOHE, __instance, DisableTOHEButtonToggle);
+            static void DisableTOHEButtonToggle()
             {
-                Main.UnlockFPS.Value = !Main.UnlockFPS.Value;
-                UpdateToggle(UnlockFPS, Translator.GetString("UnlockFPS") + ": ", Main.UnlockFPS.Value);
-                Application.targetFrameRate = Main.UnlockFPS.Value ? 165 : 60;
-                Logger.SendInGame(string.Format(Translator.GetString("FPSSetTo"), Application.targetFrameRate));
+                Harmony.UnpatchAll();
+                Main.Instance.Unload();
             }
+        }
+    }
+}
+
+[HarmonyPatch(typeof(OptionsMenuBehaviour), nameof(OptionsMenuBehaviour.Close))]
+public static class OptionsMenuBehaviourClosePatch
+{
+    public static void Postfix()
+    {
+        if (ClientOptionItem.CustomBackground != null)
+        {
+            ClientOptionItem.CustomBackground.gameObject.SetActive(false);
         }
     }
 }
