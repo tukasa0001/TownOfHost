9ebc8d478e1d6e0e92d2b8ab16f146f341a8e39c
KARPED1EM <leever.zzz@gmail.com>
2023-04-07 19:13:36 +0800

    Update Version

--- main.cs
+++ main.cs
@@ -36,7 +36,7 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = "开源社区项目，仅供交流学习";
     public static readonly string BANNEDWORDS_FILE_PATH = "./TOHE_DATA/BanWords.txt";
     public const string PluginGuid = "com.karped1em.townofhostedited";
-    public const string PluginVersion = "2.2.5";
+    public const string PluginVersion = "2.2.6";
     public const int PluginCreate = 17;
     public Harmony Harmony { get; } = new Harmony(PluginGuid);
     public static Version version = Version.Parse(PluginVersion);
