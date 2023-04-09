a839b1b800320d88fb353867a85718f9c9ce557b
KARPED1EM <leever.zzz@gmail.com>
2023-04-09 20:34:21 +0800

    Github Button

--- Patches/MainMenuManagerPatch.cs
+++ Patches/MainMenuManagerPatch.cs
@@ -90,6 +90,17 @@ public class MainMenuManagerPatch
         updateButtonSprite.size *= 1.5f;
         updateButton.SetActive(false);
 
+#if RELEASE
+            //フリープレイの無効化
+            var freeplayButton = GameObject.Find("/MainUI/FreePlayButton");
+            if (freeplayButton != null)
+            {
+                freeplayButton.GetComponent<PassiveButton>().OnClick = new();
+                freeplayButton.GetComponent<PassiveButton>().OnClick.AddListener((Action)(() => Application.OpenURL("https://github.com/KARPED1EM/TownOfHostEdited")));
+                __instance.StartCoroutine(Effects.Lerp(0.01f, new Action<float>((p) => freeplayButton.transform.GetChild(0).GetComponent<TMPro.TMP_Text>().SetText("GitHub"))));
+            }
+#endif
+
         if (Main.IsAprilFools) return;
 
         var bottomTemplate = GameObject.Find("InventoryButton");
