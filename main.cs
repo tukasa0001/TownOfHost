14d1fb15f2103e2f49760c780f8433a965e86595
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-04-21 15:02:51 +0900

    色辞書に取り残された役職カラーを削除

--- main.cs
+++ main.cs
@@ -184,8 +184,6 @@ namespace TownOfHost
                     {CustomRoles.Watcher, "#800080"},
                     //特殊クルー役職
                     {CustomRoles.NiceWatcher, "#800080"}, //ウォッチャーの派生
-                    {CustomRoles.SabotageMaster, "#0000ff"},
-                    {CustomRoles.Snitch, "#b8fb4f"},
                     {CustomRoles.CSchrodingerCat, "#ffffff"}, //シュレディンガーの猫の派生
                     //ニュートラル役職
                     {CustomRoles.SchrodingerCat, "#696969"},
