7f138a182fe720b3686f1c8fef31b74f3164bb4f
そうくん <daoyecangma@gmail.com>
2023-05-15 19:38:14 +0900

    GeneralOptionを追加

--- Roles/Core/RoleBase.cs
+++ Roles/Core/RoleBase.cs
@@ -362,4 +362,14 @@ public abstract class RoleBase : IDisposable
 
     protected static AudioClip GetIntroSound(RoleTypes roleType) =>
         RoleManager.Instance.AllRoles.Where((role) => role.Role == roleType).FirstOrDefault().IntroSound;
+
+    protected enum GeneralOption
+    {
+        Cooldown,
+        KillCooldown,
+        CanVent,
+        ImpostorVision,
+        CanUseSabotage,
+        CanCreateMadmate,
+    }
 }
