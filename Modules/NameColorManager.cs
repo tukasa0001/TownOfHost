b491bb79642cb772d09037be395de27a81298bc1
Loonie <51543683+Loonie-Toons@users.noreply.github.com>
2023-06-21 12:43:26 -0400

    Color player names with role color for ghosts
    Colors the names of every player with their role color when the player is dead.
    Visual change that looks nice.

--- Modules/NameColorManager.cs
+++ Modules/NameColorManager.cs
@@ -30,6 +30,7 @@ namespace TownOfHost
         {
             return seer == target
                 || target.Is(CustomRoles.GM)
+                || (Main.PlayerStates[seer.Data.PlayerId].IsDead && Options.GhostCanSeeOtherRoles.GetBool()) //seer.Data.IsDead
                 || (seer.Is(CustomRoleTypes.Impostor) && target.Is(CustomRoleTypes.Impostor))
                 || Mare.KnowTargetRoleColor(target, isMeeting);
         }
