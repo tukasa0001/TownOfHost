baa5035bdb45617edeb51575085bb787d5315ad9
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-06-29 16:31:37 +0900

    ペースト時に消し忘れたコメントを削除

--- Attributes/PluginModuleInitializerAttribute.cs
+++ Attributes/PluginModuleInitializerAttribute.cs
@@ -2,7 +2,7 @@ namespace TownOfHost.Attributes;
 
 /// <summary>
 /// <see cref="Main.Load"/>で起動時に初期化するメソッドに使う<br/>
-/// staticメソッドの前に [PluginModuleInitializerAttribute] と付けると起動時に自動で呼び出される<br/>
-/// [PluginModuleInitializerAttribute(InitializePriority.High)] のようにすることで呼び出される順番を指定できる
+/// staticメソッドの前に [PluginModuleInitializer] と付けると起動時に自動で呼び出される<br/>
+/// [PluginModuleInitializer(InitializePriority.High)] のようにすることで呼び出される順番を指定できる
 /// </summary>
 public sealed class PluginModuleInitializerAttribute : InitializerAttribute<PluginModuleInitializerAttribute> { }
