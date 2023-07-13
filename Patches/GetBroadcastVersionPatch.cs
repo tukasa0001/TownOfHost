9204be7a793ed5514a1c74f4bc4c41ad4128bebc
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-07-13 09:49:55 +0900

    ローカル部屋ではバージョン情報を書き換えないよう変更

--- Patches/GetBroadcastVersionPatch.cs
+++ Patches/GetBroadcastVersionPatch.cs
@@ -7,6 +7,10 @@ public static class GetBroadcastVersionPatch
 {
     public static bool Prefix(ref int __result)
     {
+        if (GameStates.IsLocalGame)
+        {
+            return true;
+        }
         __result = Constants.GetVersion(2222, 0, 0, 0);
         return false;
     }
