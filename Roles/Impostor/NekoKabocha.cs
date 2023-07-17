fcbf9c89c5c24d19fef73743cac5021e1a9e1101
Masami <103577031+Masami4711@users.noreply.github.com>
2023-07-17 18:09:48 +0900

    2人盤面でジャッカルが勝てなくなる問題を回避

--- Roles/Impostor/NekoKabocha.cs
+++ Roles/Impostor/NekoKabocha.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AmongUs.GameOptions;
 using TownOfHost.Modules;
 using TownOfHost.Roles.Core;
@@ -55,6 +56,11 @@ public sealed class NekoKabocha : RoleBase, IImpostor, INekomata
         // 普通のキルじゃない．もしくはキルを行わない時はreturn
         if (info.IsAccident || info.IsSuicide || !info.CanKill || !info.DoKill)
         {
+            return;
+        }
+        if (Main.AllAlivePlayerControls.All(pc => pc == info.AttemptKiller || pc == info.AttemptTarget))
+        {
+            logger.Info("2人盤面なので仕返しされません");
             return;
         }
         // 殺してきた人を殺し返す
