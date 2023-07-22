87ac9be681010c2670f96c63c6d71e4444a22e08
そうくん <daoyecangma@gmail.com>
2023-07-23 02:21:14 +0900

    TMPTemplateクラスを追加

---
+++ Templates/TMPTemplate.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+namespace TownOfHost.Templates;
+
+public sealed class TMPTemplate
+{
+    private static TextMeshPro baseTMP;
+    public static void SetBase(TextMeshPro tmp)
+    {
+        if (baseTMP != null) return;
+
+        baseTMP = Object.Instantiate(tmp);
+        Object.Destroy(baseTMP.GetComponent<AspectPosition>());
+        Object.DontDestroyOnLoad(baseTMP);
+        baseTMP.gameObject.SetActive(false);
+    }
+    public static TextMeshPro Create(
+        string text = null,
+        Color? color = null,
+        float? fontSize = null,
+        TextAlignmentOptions? alignment = null,
+        bool setActive = false,
+        Transform parent = null
+        )
+    {
+        var replicatedObject = parent == null
+            ? Object.Instantiate(baseTMP)
+            : Object.Instantiate(baseTMP, parent);
+        replicatedObject.text = text ?? "";
+        replicatedObject.color = color ?? Color.white;
+        replicatedObject.fontSize =
+        replicatedObject.fontSizeMax =
+        replicatedObject.fontSizeMin = fontSize ?? baseTMP.fontSize;
+        replicatedObject.alignment = alignment ?? TextAlignmentOptions.Center;
+
+        replicatedObject.gameObject.SetActive(setActive);
+
+        return replicatedObject;
+    }
+}
