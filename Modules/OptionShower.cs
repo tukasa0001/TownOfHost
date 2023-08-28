ea67edb89e465e97fa8588c8087ffe2f73e17f50
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-08-29 00:19:31 +0900

    ShowChildrenでdeepが0のときに例外が出るのを修正

--- Modules/OptionShower.cs
+++ Modules/OptionShower.cs
@@ -105,8 +105,11 @@ namespace TownOfHost
             foreach (var opt in option.Children.Select((v, i) => new { Value = v, Index = i + 1 }))
             {
                 if (opt.Value.Name == "Maximum") continue; //Maximumの項目は飛ばす
-                sb.Append(string.Concat(Enumerable.Repeat(Utils.ColorString(color, "┃"), deep - 1)));
-                sb.Append(Utils.ColorString(color, opt.Index == option.Children.Count ? "┗ " : "┣ "));
+                if (deep > 0)
+                {
+                    sb.Append(string.Concat(Enumerable.Repeat(Utils.ColorString(color, "┃"), deep - 1)));
+                    sb.Append(Utils.ColorString(color, opt.Index == option.Children.Count ? "┗ " : "┣ "));
+                }
                 sb.Append($"{opt.Value.GetName()}: {opt.Value.GetString()}\n");
                 if (opt.Value.GetBool()) ShowChildren(opt.Value, ref sb, color, deep + 1);
             }
