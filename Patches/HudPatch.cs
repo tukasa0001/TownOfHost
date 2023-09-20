c06dbbb0d9bf979871ad55e5b2af3228b5fd372c
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2023-09-20 22:27:04 +0900

    マップボタンの判定にCanUseSabotageButtonを使用

--- Patches/HudPatch.cs
+++ Patches/HudPatch.cs
@@ -213,7 +213,7 @@ namespace TownOfHost
             if (opts.Mode is MapOptions.Modes.Normal or MapOptions.Modes.Sabotage)
             {
                 var player = PlayerControl.LocalPlayer;
-                if (player.Is(CustomRoleTypes.Impostor) || (player.Is(CustomRoles.Jackal) && Jackal.CanUseSabotage) || player.Is(CustomRoles.Egoist))
+                if (player.GetRoleClass() is IKiller killer && killer.CanUseSabotageButton())
                     opts.Mode = MapOptions.Modes.Sabotage;
                 else
                     opts.Mode = MapOptions.Modes.Normal;
