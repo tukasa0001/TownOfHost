5540b0fbf3468a729a8c25a49c2e4848a0a10b02
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-10-01 18:06:25 +0900

    新しいMod用プロトコルバージョンに変更

--- Patches/GetBroadcastVersionPatch.cs
+++ Patches/GetBroadcastVersionPatch.cs
@@ -5,13 +5,12 @@ namespace TownOfHost.Patches;
 [HarmonyPatch(typeof(Constants), nameof(Constants.GetBroadcastVersion))]
 public static class GetBroadcastVersionPatch
 {
-    public static bool Prefix(ref int __result)
+    public static void Postfix(ref int __result)
     {
         if (GameStates.IsLocalGame)
         {
-            return true;
+            return;
         }
-        __result = Constants.GetVersion(2222, 0, 0, 0);
-        return false;
+        __result += 25;
     }
 }
