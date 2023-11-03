1d1cb7c24168886b43719159a4690fadc585962d
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-11-04 00:35:15 +0900

    キノコカオス効果時間変更のオプションと翻訳を追加

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -281,6 +281,7 @@ namespace TownOfHost
         public static OptionItem SabotageTimeControl;
         public static OptionItem PolusReactorTimeLimit;
         public static OptionItem AirshipReactorTimeLimit;
+        public static OptionItem FungleMushroomMixupDuration;
 
         // サボタージュのクールダウン変更
         public static OptionItem ModifySabotageCooldown;
@@ -453,6 +454,9 @@ namespace TownOfHost
                 .SetValueFormat(OptionFormat.Seconds)
                 .SetGameMode(CustomGameMode.Standard);
             AirshipReactorTimeLimit = FloatOptionItem.Create(100802, "AirshipReactorTimeLimit", new(1f, 90f, 1f), 60f, TabGroup.MainSettings, false).SetParent(SabotageTimeControl)
+                .SetValueFormat(OptionFormat.Seconds)
+                .SetGameMode(CustomGameMode.Standard);
+            FungleMushroomMixupDuration = FloatOptionItem.Create(100804, "FungleMushroomMixupDuration", new(1f, 20f, 1f), 10f, TabGroup.MainSettings, false).SetParent(SabotageTimeControl)
                 .SetValueFormat(OptionFormat.Seconds)
                 .SetGameMode(CustomGameMode.Standard);
 
