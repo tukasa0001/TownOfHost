151c22843f47e8eb39d3a8edde09f69a0fcc7cd0
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2024-03-21 20:37:39 +0900

    サポートアモアスバージョンを2024.3.5に変更

--- main.cs
+++ main.cs
@@ -53,7 +53,7 @@ namespace TownOfHost
         public const string PluginGuid = "com.emptybottle.townofhost";
         public const string PluginVersion = "5.1.6";
         // サポートされている最低のAmongUsバージョン
-        public static readonly string LowestSupportedVersion = "2023.10.24";
+        public static readonly string LowestSupportedVersion = "2024.3.5";
         // このバージョンのみで公開ルームを無効にする場合
         public static readonly bool IsPublicAvailableOnThisVersion = false;
         public Harmony Harmony { get; } = new Harmony(PluginGuid);
