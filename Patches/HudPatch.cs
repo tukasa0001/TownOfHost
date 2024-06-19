2360f1e0bbd47433aeb2d819c206ac1db3c740de
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2024-06-20 01:04:20 +0900

    GameSettingsがなくなったため削除

--- Patches/HudPatch.cs
+++ Patches/HudPatch.cs
@@ -42,12 +42,6 @@ namespace TownOfHost
                     player.Collider.offset = new Vector2(0f, -0.3636f);
                 }
             }
-            if (GameStates.IsLobby)
-            {
-                __instance.GameSettings.text = OptionShower.GetText();
-                __instance.GameSettings.fontSizeMin =
-                __instance.GameSettings.fontSizeMax = (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Japanese || Main.ForceJapanese.Value) ? 1.05f : 1.2f;
-            }
             //ゲーム中でなければ以下は実行されない
             if (!AmongUsClient.Instance.IsGameStarted) return;
 
