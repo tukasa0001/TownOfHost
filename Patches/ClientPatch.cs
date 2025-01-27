9c5330d496fa5fbc0888e7223920bb97bffbd5c8
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2025-01-27 18:10:35 +0900

    SendOrDisconnectPatchでパケット分割を実施するよう変更

--- Patches/ClientPatch.cs
+++ Patches/ClientPatch.cs
@@ -230,15 +230,128 @@ namespace TownOfHost
             return true;
         }
         [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.SendOrDisconnect)), HarmonyPrefix]
-        public static void SendOrDisconnectPatch(InnerNetClient __instance, MessageWriter msg)
+        public static bool SendOrDisconnectPatch(InnerNetClient __instance, MessageWriter msg)
         {
+            //分割するサイズ。大きすぎるとリトライ時不利、小さすぎると受信パケット取りこぼしが発生しうる。
+            var limitSize = 500;
+
             if (DebugModeManager.IsDebugMode)
             {
                 Logger.Info($"SendOrDisconnectPatch:Packet({msg.Length}) ,SendOption:{msg.SendOption}", "InnerNetClient");
             }
-            else if (msg.Length > 1000)
+            else if (msg.Length > limitSize)
             {
                 Logger.Info($"SendOrDisconnectPatch:Large Packet({msg.Length})", "InnerNetClient");
+            }
+
+            if (!Options.FixSpawnPacketSize.GetBool()) return true;
+
+            //ラージパケットを分割(9人以上部屋で落ちる現象の対策コード)
+
+            //メッセージが大きすぎる場合は分割して送信を試みる
+            if (msg.Length > limitSize)
+            {
+                var writer = MessageWriter.Get(msg.SendOption);
+                var reader = MessageReader.Get(msg.ToByteArray(false));
+
+                //Tagレベルの処理
+                while (reader.Position < reader.Length)
+                {
+                    //Logger.Info($"SendOrDisconnectPatch:reader {reader.Position} / {reader.Length}", "InnerNetClient");
+
+                    var partMsg = reader.ReadMessage();
+                    var tag = partMsg.Tag;
+
+                    //Logger.Info($"SendOrDisconnectPatch:partMsg Tag={tag} Length={partMsg.Length}", "InnerNetClient");
+
+                    //TagがGameData,GameDataToの場合のみ分割処理
+                    //それ以外では多分分割しなくても問題ない
+                    if (tag is 5 or 6 && partMsg.Length > limitSize)
+                    {
+                        //分割を試みる
+                        DivideLargeMessage(__instance, writer, partMsg);
+                    }
+                    else
+                    {
+                        //そのまま追加
+                        WriteMessage(writer, partMsg);
+                    }
+
+                    //送信サイズが制限を超えた場合は送信
+                    if (writer.Length > limitSize)
+                    {
+                        Send(__instance, writer);
+                        writer.Clear(writer.SendOption);
+                    }
+                }
+
+                //残りの送信
+                if (writer.HasBytes(7))
+                {
+                    Send(__instance, writer);
+                }
+
+                writer.Recycle();
+                reader.Recycle();
+                return false;
+            }
+            return true;
+        }
+        private static void DivideLargeMessage(InnerNetClient __instance, MessageWriter writer, MessageReader partMsg)
+        {
+            var tag = partMsg.Tag;
+            var GameId = partMsg.ReadInt32();
+            var ClientId = -1;
+
+            //元と同じTagを開く
+            writer.StartMessage(tag);
+            writer.Write(GameId);
+            if (tag == 6)
+            {
+                ClientId = partMsg.ReadPackedInt32();
+                writer.WritePacked(ClientId);
+            }
+
+            //Flag単位の処理
+            while (partMsg.Position < partMsg.Length)
+            {
+                var subMsg = partMsg.ReadMessage();
+                var subLength = subMsg.Length;
+
+                //加算すると制限を超える場合は先に送信
+                if (writer.Length + subLength > 500)
+                {
+                    writer.EndMessage();
+                    Send(__instance, writer);
+                    //再度Tagを開く
+                    writer.Clear(writer.SendOption);
+                    writer.StartMessage(tag);
+                    writer.Write(GameId);
+                    if (tag == 6)
+                    {
+                        writer.WritePacked(ClientId);
+                    }
+                }
+                //メッセージの出力
+                WriteMessage(writer, subMsg);
+            }
+            writer.EndMessage();
+        }
+
+        private static void WriteMessage(MessageWriter writer, MessageReader reader)
+        {
+            writer.Write((ushort)reader.Length);
+            writer.Write(reader.Tag);
+            writer.Write(reader.ReadBytes(reader.Length));
+        }
+
+        private static void Send(InnerNetClient __instance, MessageWriter writer)
+        {
+            Logger.Info($"SendOrDisconnectPatch: SendMessage Length={writer.Length}", "InnerNetClient");
+            var err = __instance.connection.Send(writer);
+            if (err != SendErrors.None)
+            {
+                Logger.Info($"SendOrDisconnectPatch: SendMessage Error={err}", "InnerNetClient");
             }
         }
         [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.SendInitialData)), HarmonyPrefix]
