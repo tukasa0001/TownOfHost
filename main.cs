ec89a6730bca7d88240da7a6b220ec5a27a3e232
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2025-03-26 21:01:09 +0900

    バージョンを5.1.11へ

--- main.cs
+++ main.cs
@@ -51,7 +51,7 @@ namespace TownOfHost
         // ==========
         //Sorry for many Japanese comments.
         public const string PluginGuid = "com.emptybottle.townofhost";
-        public const string PluginVersion = "5.1.10";
+        public const string PluginVersion = "5.1.11";
         // サポートされている最低のAmongUsバージョン
         public static readonly string LowestSupportedVersion = "2025.3.25";
         // このバージョンのみで公開ルームを無効にする場合
